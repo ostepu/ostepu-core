519eda0a7e27c794559ad1b26df238905f992816
Florian Lücke <florian.luecke@gmail.com>
2013-11-02 21:48:17 +0100

    Renamed print to __toString

--- include/HTMLTag.php
+++ include/HTMLTag.php
@@ -1,175 +1,175 @@
 <?php
 /**
- * HTMLTag class
- * abstract class that represents an html element
- */
+* HTMLTag class
+* An abstract class that represents an html element. Provides attributes for all global HTML attributes. Provides a way to output these to the user.
+*/
 abstract class HTMLTag {
 	/**
-	 * Specifies a shortcut key to activate/focus the element.
-	 */
+	* Specifies a shortcut key to activate/focus the element.
+	*/
 	protected $accesskey;
 	
 	/**
-	 * Specifies one or more classnames for the element (refers to a class in a style sheet).
-	 */
+	* Specifies one or more classnames for the element (refers to a class in a style sheet).
+	*/
 	protected $class;
 	
 	/**
-	 * Specifies the text direction for the content in the element
-	 */
+	* Specifies the text direction for the content in the element
+	*/
 	protected $dir;
 	
 	/**
-	 * Specifies a unique id for the element.
-	 */
+	* Specifies a unique id for the element.
+	*/
 	protected $id;
 	
 	/**
-	 * Specifies the language of the element's content.
-	 */
+	* Specifies the language of the element's content.
+	*/
 	protected $lang;
 	
 	
 	/**
-	 * Specifies the tabbing order of an element.
-	 */
+	* Specifies the tabbing order of an element.
+	*/
 	protected $tabindex;
 	
 	/**
-	 * Specifies extra information about the element.
-	 */
+	* Specifies extra information about the element.
+	*/
 	protected $title; 
 	
 	
 	/**
-	 * Getter for the accesskey attribute.
-	 *
-	 * @return The current accesskey of the element.
-	 */
+	* Getter for the accesskey attribute.
+	*
+	* @return The current accesskey of the element.
+	*/
 	public function getAccesskey() {
 		return $this->accesskey;
 	}
 	
 	/**
-	 * Setter for the accesskey attribute.
-	 *
-	 * @param $accesskey The new acesskey for the html element.
-	 */
+	* Setter for the accesskey attribute.
+	*
+	* @param $accesskey The new acesskey for the html element.
+	*/
 	public function setAccesskey($accesskey) {
 		$this->accesskey = $accesskey;
 	}
 	
 	/**
-	 * Getter for the class attribute.
-	 *
-	 * @return The current class of the element.
-	 */
+	* Getter for the class attribute.
+	*
+	* @return The current class of the element.
+	*/
 	public function getClass() {
 		return $this->class;
 	}
 
 	/**
-	 * Setter for the class attribute.
-	 *
-	 * @param $class The new class of the element.
-	 */
+	* Setter for the class attribute.
+	*
+	* @param $class The new class of the element.
+	*/
 	public function setClass($class) {
 		$this->class = $class;
 	}
 
 	/**
-	 * Getter for the dir attribute.
-	 *
-	 * @return The currend text direction of the element.
-	 */
+	* Getter for the dir attribute.
+	*
+	* @return The currend text direction of the element.
+	*/
 	public function getDir() {
 		return $this->dir;
 	}
 
 	/**
-	 * Setter for the dir attribute.
-	 * 
-	 * @param $dir The new text direction in the element.
-	 */
+	* Setter for the dir attribute.
+	* 
+	* @param $dir The new text direction in the element.
+	*/
 	public function setDir($dir) {
 		$this->dir = $dir;
 	}
 
 	/**
-	 * Getter for the id attribute.
-	 *
-	 * @return The current id of the Element.
-	 */
+	* Getter for the id attribute.
+	*
+	* @return The current id of the Element.
+	*/
 	public function getId() {
 		return $this->id;
 	}
 
 	/**
-	 * Setter for the id attribute.
-	 * 
-	 * @param $id The new id of the element.
-	 */
+	* Setter for the id attribute.
+	* 
+	* @param $id The new id of the element.
+	*/
 	public function setId($id) {
 		$this->id = $id;
 	}
 
 	/**
-	 * Getter for the lang attribute.
-	 * 
-	 * @return The current laguage ot the element.
-	 */
+	* Getter for the lang attribute.
+	* 
+	* @return The current laguage ot the element.
+	*/
 	public function getLang() {
 		return $this->lang;
 	}
 
 	/**
-	 * Setter for the lang attribute.
-	 *
-	 * @param $lang The new language of the element;
-	 */
+	* Setter for the lang attribute.
+	*
+	* @param $lang The new language of the element;
+	*/
 	public function setLang($lang) {
 		$this->lang = $lang;
 	}
 
 	/**
-	 * Getter for the tabindex attribute.
-	 *
-	 * @return The current tabindex of the element.
-	 */
+	* Getter for the tabindex attribute.
+	*
+	* @return The current tabindex of the element.
+	*/
 	public function getTabindex() {
 		return $this->tabindex;
 	}
 
 	/**
-	 * Setter for the tabindex attribute.
-	 * 
-	 * @param $tabindex The new tabindex of the element.
-	 */
+	* Setter for the tabindex attribute.
+	* 
+	* @param $tabindex The new tabindex of the element.
+	*/
 	public function setTabindex($tabindex) {
 		$this->tabindex = $tabindex;
 	}
 
 	/**
-	 * Setter for the tile attribute.
-	 */
+	* Setter for the tile attribute.
+	*/
 	public function getTitle() {
 		return $this->title;
 	}
 
 	/**
-	 * Setter for the title attribute.
-	 * Sets the title attribute of the html tag.
-	 *
-	 * @param $title the new title of the html tag
-	 */
+	* Setter for the title attribute.
+	* Sets the title attribute of the html tag.
+	*
+	* @param $title the new title of the html tag
+	*/
 	public function setTitle($title) {
 		$this->title = $title;
 	}
 	
 	/**
-	 * A method to output the tag.
-	 */
-	public function print() {
+	* A method to output the tag.
+	*/
+	public function __toString() {
 		if ($this->accesskey != NULL && $this->accesskey != "") {
 			echo "accesskey=\"" . $this->accesskey . "\" ";
 		}
