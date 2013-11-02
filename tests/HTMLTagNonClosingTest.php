f2befd55beae7ea92fcab69f2bc050953054dd68
Florian Lücke <florian.luecke@gmail.com>
2013-11-03 00:48:00 +0100

    Documentation

--- tests/HTMLTagNonClosingTest.php
+++ tests/HTMLTagNonClosingTest.php
@@ -1,5 +1,9 @@
 <?php
 include dirname(__FILE__) . '/../include/HTMLTagNonClosing.php';
+
+/**
+ * Dummy class for testing the abstract class HTMLTagNonClosing
+ */
 class HTMLTagNonClosingTest extends HTMLTagNonClosing {
 	
 }
