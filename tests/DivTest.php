a248568ca24390f01b281874ba50908e448de1f6
Florian Lücke <florian.luecke@gmail.com>
2013-11-03 20:28:42 +0100

    fixes the include

--- tests/DivTest.php
+++ tests/DivTest.php
@@ -1,7 +1,9 @@
 <?php
-require_once dirname(__FILE__) . '../include/Div.php';
+require_once dirname(__FILE__) . '/../include/Div.php';
 
 $d = new Div();
+
+assert_options(ASSERT_BAIL);
 
 assert($d->__toString() == "<div>\n    \n</div>");
 
