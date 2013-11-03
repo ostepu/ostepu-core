08142b489e7f957f391e814ca60e56437cf41f8f
Florian Lücke <florian.luecke@gmail.com>
2013-11-03 20:59:23 +0100

    Header.php now uses the new classes to genrate its html source code

--- Header.php
+++ Header.php
@@ -1,37 +1,88 @@
 <?php
-    class Header {
+include 'include/HTML.php';
+class Header {
 	private $title;
 	private $extraInfo;
 	private $username;
 	private $userid;
 
 	function __construct($title, $extraInfo, $username, $userid) {
-	    $this->title = $title;
-	    $this->extraInfo = $extraInfo;
-	    $this->username = $username;
-	    $this->userid = $userid;
+		$this->title = $title;
+		$this->extraInfo = $extraInfo;
+		$this->username = $username;
+		$this->userid = $userid;
 	}
 	
 	public function getTitle() {
-	    return $this->title;
+		return $this->title;
 	}
 
 	public function show() {
-	    echo '<div id="header">';
-	    echo '    <div class="session-info">';
-	    echo '        <div class="session-title">' . $this->title . '</div>';
-	    echo '        <a class="change-session" href="#">Veranstaltung wechseln</a>';
-	    echo '    </div>';
-	    echo '    <div class="session-points">';
-	    echo '        <div class="points-huge">75%</div>';
-	    echo '        <div class="total-points">Gesamtpunkte</div>';
-	    echo '    </div>';
-	    echo '    <div class="user-info">';
-	    echo '        <div class="user-name">' . $this->username . '</div>';
-	    echo '        <div class="user-id">' . $this->userid . '</div>';
-	    echo '        <a class="log-out" href="#">Ausloggen</a>';
-	    echo '    </div>';
-	    echo '</div>';
+		/**
+		 * generate session info
+		 */
+		$sessionTitle = new Div();
+		$sessionTitle->setClass("session-title")
+					 ->setContent($this->title);
+		
+		$changeSession = new A();
+		$changeSession->setClass("change-session")
+					  ->setHref("#")
+					  ->setContent("Veranstaltung wechseln");
+		
+		$sessionInfo = new Div();
+		$sessionInfo->setClass("session-info")
+					->setContent($sessionTitle)
+					->addContent($changeSession);
+		
+		/**
+		 * generate session points
+		 */
+		$pointsHuge = new Div();
+		$pointsHuge->setClass("points-huge")
+				   ->setContent("75%");
+		
+		$totalPoints = new Div();
+		$totalPoints->setClass("total-points")
+					->setContent("Gesamtpunkte");
+		
+		$sessionPoints = new Div();
+		$sessionPoints->setClass("session-points")
+					  ->setContent($pointsHuge)
+					  ->addContent($totalPoints);
+		
+		/**
+		 * generate user-info
+		 */
+		$userName = new Div();
+		$userName->setClass("user-name")
+				 ->setContent($this->username);
+		
+		$userId = new Div();
+		$userId->setClass("user-id")
+			   ->setContent($this->userid);
+		
+		$logOut = new A();
+		$logOut->setClass("log-out")
+			   ->setHref("#")
+			   ->setContent("Ausloggen");
+		
+		$userInfo = new Div();
+		$userInfo->setClass("user-info")
+				 ->addContent($userName)
+				 ->addContent($userId)
+				 ->addContent($logOut);
+		
+		/**
+		 * generate header
+		 */
+		$header = new Div();
+		$header->setContent($sessionInfo)
+			   ->addContent($sessionPoints)
+			   ->addContent($userInfo)
+			   ->setId("header");
+		
+		print $header;
 	}
-    }
+}
 ?>
