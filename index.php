6a1e77a1f74fe0b99d25cef8f8548b8b1ca517d5
Florian Lücke <florian.luecke@gmail.com>
2013-11-04 19:04:18 +0100

    changes some css class names

--- index.php
+++ index.php
@@ -21,22 +21,22 @@
     $h->show();
 ?>
 	    <div id="content-wrapper">
-		<div class="issue-wrapper">
-		    <div class="issue-header">
-			<div class="issue-title">Serie 2</div>
-			<div class="issue-end">23.11.2013 20:003</div>
-			<div class="issue-percent">100%</div>			
+		<div class="exercise-sheet-wrapper">
+		    <div class="exercise-sheet-header">
+			<div class="exercise-sheet-title">Serie 2</div>
+			<div class="exercise-sheet-end">23.11.2013 20:003</div>
+			<div class="exercise-sheet-percent">100%</div>			
 		    </div>
 
-		    <div class="issue-body-wrapper">
-			<div class="issue-body-left issue-body">
+		    <div class="exercise-sheet-body-wrapper">
+			<div class="exercise-sheet-body-left exercise-sheet-body">
 			    <ol class="exercise-list">
 				<li><div class="exercise-type">Normal</div> <div class="exercise-points">0/10</div> </li>
 				<li><div class="exercise-type">Bonus</div> <div class="exercise-points">0/10</div> </li>				    
 			    </ol>
 			</div>
 
-			<div class="issue-body-extras issue-body">
+			<div class="exercise-sheet-body-extras exercise-sheet-body">
 			    <ol class="body-options">
 				<li><a class="body-option" href="#">Serie ansehen</a></li>
 				<li><a class="body-option" href="#">Musterlösung</a></li>
@@ -44,8 +44,8 @@
 				<li><a class="body-option" href="#">Gruppe austreten</a></li>
 			    </ol>
 			</div>
-		    </div> <!-- end: issue-body-wrapper -->
-		</div> <!-- end: issue-wrapper -->
+		    </div> <!-- end: exercise-sheet-body-wrapper -->
+		</div> <!-- end: exercise-sheet-wrapper -->
 	    </div> <!-- end: content-wrapper -->
 	</div>
     </body>
