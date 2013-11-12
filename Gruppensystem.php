250e9b649217157b90b466e4eaff20ccc3a866e5
Florian Lücke <florian.luecke@gmail.com>
2013-11-12 16:55:25 +0100

    adds group management site

---
+++ Gruppensystem.php
@@ -0,0 +1,126 @@
+<?php
+include 'Header.php';
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="CSSReset.css"> 
+    <link rel="stylesheet" type="text/css" href="Uebungsplattform.css"> 
+    <title>Übungsplattform</title>
+</head>
+<body>
+    <div id="body-wrapper">
+
+        <?php
+            $h = new Header("Datenstrukturen",
+                            "",
+                            "Florian Lücke",
+                            "211221492", 
+                            "75%");
+
+            $h->setBackURL("index.php")
+              ->setBackTitle("zur Veranstaltung");
+
+            $h->show();
+        ?>
+
+        <div id="content-wrapper">
+            <div class="exercise-sheet-wrapper">
+                <div class="exercise-sheet-header">
+                    <div class="exercise-sheet-title">Gruppe verwalten</div>
+                    <div class="exercise-sheet-end">
+                        <a href="#" class="body-option">verlassen</a>
+                    </div>   
+                </div>
+
+                <div class="exercise-sheet-body-wrapper">
+                    <div class="exercise-sheet-body-left exercise-sheet-body">
+                        <ol class="exercise-list invitation-list">
+                            <li >
+                                Jörg Baumgarten
+                                <a href="#" class="body-option deny-button">
+                                    hinauswerfen
+                                </a>
+                            </li>
+                            <li >
+                                Lisa Dietrich
+                                <a href="#" class="body-option deny-button">
+                                    hinauswerfen
+                                </a>
+                            </li>
+                        </ol>
+                    </div>
+                </div> <!-- end: exercise-sheet-body-wrapper -->
+            </div> <!-- end: exercise-sheet-wrapper -->
+
+            <div class="exercise-sheet-wrapper">
+                <div class="exercise-sheet-header">
+                    <div class="exercise-sheet-title">Erstellen</div>
+                    <div class="exercise-sheet-end">
+                        <a href="#" class="body-option">einladen</a>
+                    </div>     
+                </div>
+
+                <div class="exercise-sheet-body-wrapper">
+                    <div class="exercise-sheet-body-left exercise-sheet-body">
+                        <ol class="exercise-list invitation-list">
+                            <li>
+                                Matrikel: <input type="text" name="ids[]" size="50">
+                            </li>
+                            <li>
+                                Matrikel: <input type="text" name="ids[]" size="50">
+                            </li>
+                            <li>
+                                Matrikel: <input type="text" name="ids[]" size="50">
+                            </li>
+                        </ol>
+                    </div>
+                </div> <!-- end: exercise-sheet-body-wrapper -->
+            </div> <!-- end: exercise-sheet-wrapper -->
+
+            <div class="exercise-sheet-wrapper">
+                <div class="exercise-sheet-header">
+                    <div class="exercise-sheet-title">Einladungen</div>     
+                </div>
+
+                <div class="exercise-sheet-body-wrapper">
+                    <div class="exercise-sheet-body-left exercise-sheet-body">
+                        <ol class="exercise-list invitation-list">
+                            <li >
+                                Peter König, Felix Schmidt
+                                <a href="#" class="body-option deny-button">
+                                    ablehnen
+                                </a>
+                                <a href="#" class="body-option accept-button">
+                                    annehmen
+                                </a>
+                            </li>
+                            <li >
+                                Till Uhlig, Ralf Busch
+                                <a href="#" class="body-option deny-button">
+                                    ablehnen
+                                </a>
+                                <a href="#" class="body-option accept-button">
+                                    annehmen
+                                </a>
+                            </li>
+                            <li >
+                                Martin Daute, Christian Elze
+                                <a href="#" class="body-option deny-button">
+                                    ablehnen
+                                </a>
+                                <a href="#" class="body-option accept-button">
+                                    annehmen
+                                </a>
+                            </li>
+                        </ol>
+                    </div>
+                </div> <!-- end: exercise-sheet-body-wrapper -->
+            </div> <!-- end: exercise-sheet-wrapper -->
+        </div>
+    </div> <!-- end: content-wrapper -->
+</div>
+</body>
+</html>
+
--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
-    include 'Header.php';
+     include 'Header.php';
 ?>
 
 <!DOCTYPE HTML>
 <html>
     <head>
-	<link rel="stylesheet" type="text/css" href="CSSReset.css">	
-	<link rel="stylesheet" type="text/css" href="Uebungsplattform.css">
-	<title>Übungsplattform</title>
+    <link rel="stylesheet" type="text/css" href="CSSReset.css"> 
+    <link rel="stylesheet" type="text/css" href="Uebungsplattform.css">
+    <title>Übungsplattform</title>
     </head>
     <body>
-	<div id="body-wrapper">
+    <div id="body-wrapper">
 
 <?php
     $h = new Header("Datenstrukturen",
@@ -20,34 +20,34 @@
                     "75%");
     $h->show();
 ?>
-	    <div id="content-wrapper">
-		<div class="exercise-sheet-wrapper">
-		    <div class="exercise-sheet-header">
-			<div class="exercise-sheet-title">Serie 2</div>
-			<div class="exercise-sheet-end">23.11.2013 20:003</div>
-			<div class="exercise-sheet-percent">100%</div>			
-		    </div>
+        <div id="content-wrapper">
+        <div class="exercise-sheet-wrapper">
+            <div class="exercise-sheet-header">
+            <div class="exercise-sheet-title">Serie 2</div>
+            <div class="exercise-sheet-end">23.11.2013 20:003</div>
+            <div class="exercise-sheet-percent">100%</div>          
+            </div>
 
-		    <div class="exercise-sheet-body-wrapper">
-			<div class="exercise-sheet-body-left exercise-sheet-body">
-			    <ol class="exercise-list">
-				<li><div class="exercise-type">Normal</div> <div class="exercise-points">0/10</div> </li>
-				<li><div class="exercise-type">Bonus</div> <div class="exercise-points">0/10</div> </li>				    
-			    </ol>
-			</div>
+            <div class="exercise-sheet-body-wrapper">
+            <div class="exercise-sheet-body-left exercise-sheet-body">
+                <ol class="exercise-list">
+                    <li><div class="exercise-type">Normal</div> <div class="exercise-points">0/10</div> </li>
+                    <li><div class="exercise-type">Bonus</div> <div class="exercise-points">0/10</div> </li>                    
+                </ol>
+            </div>
 
-			<div class="exercise-sheet-body-extras exercise-sheet-body">
-			    <ol class="body-options">
-				<li><a class="body-option" href="#">Serie ansehen</a></li>
-				<li><a class="body-option" href="#">Musterlösung</a></li>
-				<li><a class="body-option" href="#">Gruppe</a></li>
-				<li><a class="body-option" href="#">Gruppe austreten</a></li>
-			    </ol>
-			</div>
-		    </div> <!-- end: exercise-sheet-body-wrapper -->
-		</div> <!-- end: exercise-sheet-wrapper -->
-	    </div> <!-- end: content-wrapper -->
-	</div>
+            <div class="exercise-sheet-body-extras exercise-sheet-body">
+                <ol class="body-options">
+                <li><a class="body-option" href="#">Serie ansehen</a></li>
+                <li><a class="body-option" href="#">Musterlösung</a></li>
+                <li><a class="body-option" href="Gruppensystem.php">Gruppe</a></li>
+                <li><a class="body-option" href="#">Gruppe verlassen</a></li>
+                </ol>
+            </div>
+            </div> <!-- end: exercise-sheet-body-wrapper -->
+        </div> <!-- end: exercise-sheet-wrapper -->
+        </div> <!-- end: content-wrapper -->
+    </div>
     </body>
 </html>
 
