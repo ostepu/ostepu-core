32ac699f78e266cd651c6df51e0c78f8d091b15e
Florian Lücke <florian.luecke@gmail.com>
2013-11-22 22:26:30 +0100

    the tutor page is now generated using the new templates

--- Tutor.php
+++ Tutor.php
@@ -1,7 +1,7 @@
 <?php
 include_once 'include/Header/Header.php';
-include_once 'include/ExerciseSheet/ExerciseSheetTutor.php';
 include_once 'include/HTMLWrapper.php';
+include_once 'include/Template.php';
 
 // construct a new header
 $h = new Header("Datenstrukturen",
@@ -12,23 +12,14 @@ $h = new Header("Datenstrukturen",
 // construct some exercise sheets
 $sheetString = file_get_contents("http://localhost/Uebungsplattform/Sheet");
 
-
-
 // convert the json string into an associative array
 $sheets = json_decode($sheetString, true);
 
-$w = new HTMLWrapper($h);
+$t = Template::WithTemplateFile('include/ExerciseSheet/ExerciseSheetTutor.template.json');
 
-$content = array();
+$t->bind($sheets);
 
-foreach ($sheets as $sheet) {
-    $ex = $sheet['exercises'];
-    $e = new ExerciseSheetTutor($sheet['name'], $ex,
-                                $sheet['exerciseSheetInfo'], $sheet['endTime']);
-
-    // wrap the element in some HTML
-    $w->insert($e);
-}
+$w = new HTMLWrapper($h, $t);
 $w->show();
 ?>
 
