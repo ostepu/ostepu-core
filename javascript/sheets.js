c18f1dfdc093ec009d45cdd40e721526c5b0f86a
Ralf Busch <ralfbusch92@gmail.com>
2013-11-26 17:33:16 +0100

    added arrows to exercise sheet headers

--- javascript/sheets.js
+++ javascript/sheets.js
@@ -1,15 +1,17 @@
 $(document).ready( function() {
-    // get all elements with class .content-header with index greater 2 and hide the first element .content-body-wrapper
-    $('.content-header:gt(2)').next('.content-body-wrapper').hide();
+    // get all elements with class .sheet-header with index greater 2 and hide the first element .content-body-wrapper
+    $('.sheet-header:gt(2)').next('.content-body-wrapper').hide();
+    $('.sheet-header:gt(2)').addClass('inactive');
     /**
      * toggle function on click to hide/show .content-header elements
      */
-    $('.content-header').click( function() {
+    $('.sheet-header').click( function() {
         // trig = event sender
         var trig = $(this);
         // toggle the next available element of .content-body-wrapper near the "trig" with duration "fast"
         trig.next('.content-body-wrapper').slideToggle('fast');
+        trig.toggleClass( 'inactive',  !trig.hasClass('inactive') );
     });
     // set mouse curser on mouse-over to pointer 
-    $('.content-header').css('cursor','pointer');
+    $('.sheet-header').css('cursor','pointer');
 });
