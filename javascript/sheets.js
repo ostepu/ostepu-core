027952333a345b20738f236dc079ea638fb99b2e
Florian Lücke <florian.luecke@gmail.com>
2013-11-29 14:06:57 +0100

    makes content-elements collapsible

--- javascript/sheets.js
+++ javascript/sheets.js
@@ -1,17 +1,22 @@
 $(document).ready( function() {
     // get all elements with class .sheet-header with index greater 2 and hide the first element .content-body-wrapper
-    $('.sheet-header:gt(2)').next('.content-body-wrapper').hide();
-    $('.sheet-header:gt(2)').addClass('inactive');
+
+    console.log($('.collapsible:gt(2)'));
+
+    $('.collapsible:gt(2)').children('.content-body-wrapper').hide();
+    $('.collapsible:gt(2)').children('.content-header').addClass('inactive');
     /**
      * toggle function on click to hide/show .content-header elements
      */
-    $('.sheet-header').click( function() {
+    $('.collapsible').children('.content-header').click( function() {
         // trig = event sender
         var trig = $(this);
         // toggle the next available element of .content-body-wrapper near the "trig" with duration "fast"
-        trig.next('.content-body-wrapper').slideToggle('fast');
-        trig.toggleClass( 'inactive',  !trig.hasClass('inactive') );
+        if (trig.parent('.collapsible').length != 0) {
+            trig.next('.content-body-wrapper').slideToggle('fast');
+            trig.toggleClass( 'inactive',  !trig.hasClass('inactive') );
+        }
     });
     // set mouse curser on mouse-over to pointer 
-    $('.sheet-header').css('cursor','pointer');
+    $('.collapsible').children('.content-header').css('cursor','pointer');
 });
