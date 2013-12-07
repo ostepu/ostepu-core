27c0e793eed6c8aefd26779bd55b65df3c50b15c
Felix Schmidt <Fiduz@Live.de>
2013-12-07 16:10:15 +0100

    adds first draft for CreateSheet

---
+++ CreateSheet.php
@@ -0,0 +1,26 @@
+<?php
+include 'include/Header/Header.php';
+include 'include/HTMLWrapper.php';
+include_once 'include/Template.php';
+
+// construct a new Header
+$h = new Header("Datenstrukturen",
+                "",
+                "Admin",
+                "211221492");
+
+$h->setBackURL("index.php")
+->setBackTitle("zur Veranstaltung");
+
+$sheetSettings = Template::WithTemplateFile('include/CreateSheet/SheetSettings.template.json');
+$sheetSettings->bind(array());
+
+$exerciseSettings = Template::WithTemplateFile('include/CreateSheet/ExerciseSettings.template.json');
+$exerciseSettings->bind(array());
+
+// wrap all the elements in some HTML and show them on the page
+$w = new HTMLWrapper($h, $sheetSettings, $exerciseSettings);
+$w->set_config_file('include/configs/config_default.json');
+$w->show();
+?>
+
