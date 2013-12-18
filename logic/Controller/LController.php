1fbe13e72141fba83330efd4d2e0dca67aa5267e
Christian Elze <christian.elze@gmail.com>
2013-12-18 09:33:53 +0100

    LController.php fixed

--- logic/Controller/LController.php
+++ logic/Controller/LController.php
@@ -1,31 +1,35 @@
 <?php 
 
 require 'Slim/Slim.php';
-include 'include/Assistants/request/createRequest.php';
+include 'include/Assistants/Request.php';
 
 \Slim\Slim::registerAutoloader();
 
 class LController
 {
-    $this->app = new \Slim\Slim();
-    $this->app->response->headers->set('Content-Type', 'application/json');
-
-
-    $links = array();
-    setLinks('config.ini');
-    $DBController = $links["DBControl"];	
-    $FSController = $links["FSControl"];
-
-    $this->app->map('/:string+', array($this, 'chooseDestination')) 
-    			->via('POST', 'GET', 'PUT', 'DELETE');
-
+    public function __construct()
+    {    
+        $this->app = new \Slim\Slim();
+        $this->app->response->headers->set('Content-Type', 'application/json');
+    
+    
+        $links = array();
+        //setLinks('config.ini');
+        //$DBController = $links["DBControl"];	
+        //$FSController = $links["FSControl"];
+    
+        $this->app->map('/:string+', array($this, 'chooseDestination')) 
+                    ->via('POST', 'GET', 'PUT', 'DELETE');
+                    
+        $this->app->run();
+    }    
     
     /**
      * pick out the config.ini file and store all addresses in $links
      *
      * @param (param)
      */
-    private function setLinks($dataName){
+    public function setLinks($dataName){
         $datei = file($dataName);
         $explodedRow = array();
 
@@ -34,70 +38,40 @@ class LController
             $links["$explodedRow[0]"] = $explodedRow[1];
         }
     }
-
-    /**
-     * select the case of the request based on the available method
-     * and send the selected on
-     * 
-     * @param (param)
-     */
-    private function chooseCaseOfRequest($method, $URL, $header, $data){
-        switch ($method) {
-            case 'POST' :
-                return createPost($URL, $header, $data);
-                break;
-                
-            case 'PUT' :			
-                return createPut($URL, $header, $data);
-                break;
-                
-            case 'GET' :
-                return createGet($URL, $header, $data);
-                break;
-                
-            case 'DELETE' :
-                return createDelete($URL, $header, $data);
-                break;
-                
-            default :
-                return "Fehler";			//Fehlermeldung
-        }
-    }
-       
-    private function chooseDestination($string){
+    
+    
+    public function chooseDestination($string){
     	$method = $this->app->request->getMethod();
     	$body = $this->app->request->getBody();
-    	$header = $this->app->request->getHeader();
+    	$header = $this->app->request->headers->all();
     	
     	if ($string[0] == "DB") {
             unset($string[0]);
-            $URI = //DB-URL;															//URI ergänzen
+            $URI = "";//DB-URL;															//URI ergänzen
             foreach ($string as $str) {
                 $URI = $URI.'/'.$str;
             }
             $this->app->response
-                ->SetBody(chooseCaseOfRequest($method, $URI, $header, $body));	
+                ->SetBody(Request::custom($method, $URI, $header, $body));	
         } elseif ($string[0] == "FS") {
             unset($string[0]);
-            $URI = //FS-URL;															//URI ergänzen
+            $URI = "";//FS-URL;															//URI ergänzen
             foreach ($string as $str) {
                 $URI = $URI.'/'.$str;
             }
             $this->app->response
-                ->SetBody(chooseCaseOfRequest($method, $URI, $header, $body));
+                ->SetBody(Request::custom($method, $URI, $header, $body));
         } else {
-            $URI = //L-URL
+            $URI = "";//L-URL
             foreach ($string as $str) {
                 $URI = $URI.'/'.$str;
             }
+            $answer = Request::custom($method, $URI, $header, $body);
             $this->app->response
-                ->SetBody(chooseCaseOfRequest($method, $URI, $header, $body));	
+                ->SetBody($answer['content']);
+            $this->app->response->setStatus($answer['status']);
         }
     }
-    
-    $this->app->run();
-    
 }
-
-new LController;
+ new LController();   
 ?>
