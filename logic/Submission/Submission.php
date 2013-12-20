a370a730a8c3c7e513f3e3b5b8dc494195b96ae2
Martin Daute <martin.daute@student.uni-halle.de>
2013-12-20 13:02:56 +0100

    Submission.php fixed

--- logic/Submission/Submission.php
+++ logic/Submission/Submission.php
@@ -1,110 +1,113 @@
 <?php 
 
 require 'Slim/Slim.php';
-include 'include/Assistants/request/createRequest.php';
-include 'include/Assistants/StructFile.php';
-include 'include/Assistants/StructSubmission.php';
+include 'include/Assistants/Request.php';
+//include 'include/Assistants/StructFile.php';
+//include 'include/Assistants/StructSubmission.php';
 
 \Slim\Slim::registerAutoloader();
 	
 class Submission
-{
-    private $dbURL = ""; //aus config lesen
-    private $fsURL = ""; //aus config lesen
+{    
+    private $lURL = ""; //aus config lesen
     
-    $this->app = new \Slim\Slim();
-    $this->app->response->headers->set('Content-Type', 'application/json');
-    
-    //AddSubmission
-    $this->app->post(':data+', array($this, 'addSubmission'));
-    
-    //EditSubmissionState
-    $this->app->put('/submission/:submissionid', 
-                    array ($this, 'editSubmissionState'));
-    
-    //deleteSubmission
-    $this->app->delete('/submission/:submissionid', 
-                    array($this, 'deleteSubmission'));
-                    
-    //LoadSubmissionAsZip
-    $this->app->get('/exerciseSheet/:sheetid/user/:userid', 
-                    array($this, 'loadSubmissionAsZip'));
-    
-    //ShowSubmissionsHistory
-    $this->app->get('/exerciseSheet/:sheetid/user/:userid/history', 
-                    array($this, 'showSubmissionsHistory'));
-               
-    //GetSubmissionURL
-    $this->app->get('/submission/:submissionid', 
-                    array($this, 'getSubmissionURL'));
-    
-    
-    
-    private function addSubmission($data){
-        //Parameter abfangen wenn $data "nicht leer"
+    public function __construct()
+    {    
+        $this->app = new \Slim\Slim();
+        $this->app->response->headers->set('Content-Type', 'application/json');
         
-        $header = $this->app->request->getHeader();
+        //AddSubmission
+        $this->app->post(':data+', array($this, 'addSubmission'));
+        
+        //EditSubmissionState
+        $this->app->put('/submission/:submissionid', 
+                        array ($this, 'editSubmissionState'));
+        
+        //deleteSubmission
+        $this->app->delete('/submission/:submissionid', 
+                        array($this, 'deleteSubmission'));
+                        
+        //LoadSubmissionAsZip
+        $this->app->get('/exerciseSheet/:sheetid/user/:userid', 
+                        array($this, 'loadSubmissionAsZip'));
+        
+        //ShowSubmissionsHistory
+        $this->app->get('/exerciseSheet/:sheetid/user/:userid/history', 
+                        array($this, 'showSubmissionsHistory'));
+                   
+        //GetSubmissionURL
+        $this->app->get('/submission/:submissionid', 
+                        array($this, 'getSubmissionURL'));
+                        
+        $this->app->run();
+    }    
+    
+    public function addSubmission($data){
+        //Parameter abfangen wenn $data "nicht leer"        
+        $header = $this->app->request->headers->all();
         $body = json_decode($this->app->request->getBody());
-        $file = json_encode($body->{'_file'});
+        $file = json_encode($body->{'_file'});      //mit oder ohne "_"?
         //Anfrage an FileSystem
-        $body->{'_file'} = createPost($fsURL, $header, $file);
-        //Anfrage an DataBase
-        $status = createPost($dbURL, $header, json_encode($body));
-        $this->app->response->setStatus($status);
+        $URL = $this->lURL.'/FS';
+        $answer = Request::custom('POST', $URL, $header, $file);
+        
+        if($answer['status'] == 200){ //nur, wenn file tatsächlich im FS gespeichert wurde
+            $body->{'_file'} = $answer['content'];
+            //Anfrage an DataBase
+            $URL = $this->lURL.'/DB';
+            $answer = Request::custom('POST', $URL, $header, json_encode($body));
+            $this->app->response->setStatus($answer['status']);
+        }
     }
     
-    private function editSubmissionState($submissionid) {
-    	
-        $header = $this->app->request->getHeader();
+    public function editSubmissionState($submissionid) {    	
+        $header = $this->app->request->headers->all();
         $body = $this->app->request->getBody();
-        $URL = $dbURL.'/submission/'.$submissionid;
-        
-        $status = createPut($URL, $header, $body);
-        $this-app->response->setStatus($status);    
+        $URL = $this->lURL.'/DB/submission/'.$submissionid;        
+        $answer = Request::custom('PUT', $URL, $header, $body);
+        $this->app->response->setStatus($answer['status']);    
     }
 
-    private function deleteSubmission($submissionid){
-    
-        $header = $this->app->request->getHeader();
+    public function deleteSubmission($submissionid){       
+        $header = $this->app->request->headers->all();
         $body = $this->app->request->getBody();
-        $URL = $dbURL.'/submission/'.$submissionid;
+        $URL = $this->lURL.'/DB/submission/'.$submissionid;        
+        $answer = Request::custum('DELETE', $URL, $header, $body);
+        $this->app->response->setStatus($answer['status']);
         
-        $status = createDelete($URL, $header, $body);
-        
-        if( $status == 200){
-            $URL = $fsURL.'/submission/'.$submissionid; 
-            $status = createDelete($URL, $header, $body);
-        }
-        
-        $this-app->response->setStatus($status);  
+        if( $answer['status'] == 200){ //nur, wenn file tatsächlich aus DB gelöscht wurde
+            $URL = $this->lURL.'/FS/submission/'.$submissionid; 
+            $answer = Request::custom('DELETE', $URL, $header, $body);
+        }             
     }
     
-    private function loadSubmissionAsZip($sheetid, $userid){                    //Annahme ZipURL in DB abrufbar
-    
-        $header = $this->app->request->getHeader();
+    public function loadSubmissionAsZip($sheetid, $userid){       //Annahme: ZipURL in DB abrufbar  
+        $header = $this->app->request->headers->all();
         $body = $this->app->request->getBody();
-        $URL = $dbURL.'/exerciseSheet/'.$sheetid.'/user/'.$userid;
-        $body = createGet($URL, $header, $body);
-        $this->app->response->setBody($body);
+        $URL = $this->lURL.'/DB/exerciseSheet/'.$sheetid.'/user/'.$userid;
+        $answer = Request::custom('GET', $URL, $header, $body);
+        $this->app->response->setBody($answer['content']);
+        $this->app->response->setStatus($answer['status']);
     }
     
-    private function showSubmissionsHistory($sheetid, $userid){
-        
-        $header = $this->app->request->getHeader();
+    public function showSubmissionsHistory($sheetid, $userid){      
+        $header = $this->app->request->headers->all();
         $body = $this->app->request->getBody();
-        $URL = $dbURL.'/exerciseSheet/'.$sheetid.'/user/'.$userid.'/history';
-        $body = createGet($URL, $header, $body);
-        $this->app->response->setBody($body);
+        $URL = $this->lURL.'/DB/exerciseSheet/'.$sheetid.'/user/'.$userid.'/history';
+        $answer = Request::custom('GET', $URL, $header, $body);
+        $this->app->response->setBody($answer['content']);
+        $this->app->response->setStatus($answer['status']);
     }
     
-    private function getSubmissionURL($submissionid){
-        
-        $header = $this->app->request->getHeader();
+    public function getSubmissionURL($submissionid){        
+        $header = $this->app->request->headers->all();
         $body = $this->app->request->getBody();
-        $URL = $dbURL.'/submission/'.$submissionid;
-        $body = createGet($URL, $header, $body);
-        $this->app->response->setBody($body);
+        $URL = $this->lURL.'/DB/submission/'.$submissionid;
+        $answer = Request::custom('GET', $URL, $header, $body);
+        $this->app->response->setBody($answer['content']);
+        $this->app->response->setStatus($answer['status']);
     }
 }
 
+new Submission();
 ?>
