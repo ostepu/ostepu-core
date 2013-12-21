1ab870ffe43496457f2e78aff46b50e5b72201c5
Ralf Busch <ralfbusch92@gmail.com>
2013-12-21 19:28:00 +0100

    fixed adding exercises capability in CreateSheet.php

--- UI/javascript/createSheet.js
+++ UI/javascript/createSheet.js
@@ -1,46 +1,68 @@
 $(document).ready( function() {
+    // rename exercise headers with correct enumeration 
+    function renameExercises() {
+        var allCollapsible = $('.collapsible').children('.content-header');
+
+        for (var i = 1; i < allCollapsible.length; i++) {
+            jQuery(allCollapsible[i]).children('.content-title')[0].innerText = "Aufgabe " + i;
+        }
+    }
 
     // if the content header contains an anchor tag prevent that clicking on it
     // will trigger the content element to collapse
-    $('.collapsible').children('.content-header').find('a').click( function(event) {
+    function stopProp(event) {
         event.stopPropagation();
 
         return false;
-    });
+    }
 
 
     // deletes the exercise and its related content-element when clicking on
     // a link with the class 'delete-exercise'
-    $('.collapsible').children('.content-header').find('.delete-exercise').click( function(event) {
+    function deleteExercise(event) {
 
         var trig = $(this);
         var container = trig.parents('.collapsible');
         container.slideToggle('fast', function() {
             container[0].parentNode.removeChild(container[0]);
 
-            var allCollapsible = $('.collapsible').children('.content-header');
-
-            for (var i = 1; i < allCollapsible.length; i++) {
-                jQuery(allCollapsible[i]).children('.content-title')[0].innerText = "Aufgabe " + i;
-            }
+            renameExercises();
         });
-    });
+    }
 
 
     // deletes the subtask and its related list-element when clicking on
     // a link with the class 'delete-subtask'
-    $('.full-width-list').find('.delete-subtask').click( function(event) {
+    function deleteSubtask(event) {
         $(this).parent().remove();
-    });
+    }
 
 
     // adds a new exercise when clicking on a link with the class 'add-exercise'
     // at the end of the page
-    $('.content-header').find('.add-exercise').click( function(event) {
-        // find last content-element
-        var lastExercise = $('.collapsible').last();
+    function addExercise(event) {
 
         // append content to last exercise
-        $(this).load( "include/CreateSheet/ExerciseSettings.template.html" ).insertAfter(lastExercise);
-    });
+        $.get("include/CreateSheet/ExerciseSettings.template.html", function (data) {
+            $("#content-wrapper").append(data);
+            
+            // map click events on new exercise
+            $('.collapsible').last().hide().fadeIn(1000);
+            $('.collapsible').last().children('.content-header').find('a').on("click",stopProp);
+            $('.collapsible').last().children('.content-header').find('.delete-exercise').on("click",deleteExercise);
+            $('.full-width-list').last().find('.delete-subtask').on("click",deleteSubtask);
+            $('.collapsible').last().children('.content-header').on("click",collapseElement);
+
+            // set mouse curser on mouse-over to pointer
+            $('.collapsible').last().children('.content-header').css('cursor','pointer');
+
+            renameExercises();
+        });
+    }
+
+    // map click events
+    $('.collapsible').children('.content-header').find('a').on("click",stopProp);
+    $('.collapsible').children('.content-header').find('.delete-exercise').on("click",deleteExercise);
+    $('.full-width-list').find('.delete-subtask').on("click",deleteSubtask);
+    $('.interactive.add').children('.content-header').find('.add-exercise').on("click",addExercise);
 });
--- UI/javascript/sheets.js
+++ UI/javascript/sheets.js
@@ -1,21 +1,25 @@
+/**
+ * toggle function on click to hide/show .content-header elements
+ */
+function collapseElement(event) {
+    // trig = event sender
+    var trig = $(this);
+    // toggle the next available element of .content-body-wrapper near the "trig" with duration "fast"
+    if (trig.parent('.collapsible').length !== 0) {
+        trig.parent().children('.content-body-wrapper, .content-footer').slideToggle('fast');
+        trig.toggleClass( 'inactive',  !trig.hasClass('inactive') );
+    }
+}
+
 $(document).ready( function() {
     // get all elements with class .sheet-header with index greater 2 and hide the first element .content-body-wrapper
 
     $('.collapsible:gt(2)').children(('.content-body-wrapper, .content-footer')).hide();
     $('.collapsible:gt(2)').children('.content-header').addClass('inactive');
-    /**
-     * toggle function on click to hide/show .content-header elements
-     */
-    $('.collapsible').children('.content-header').click( function(event) {
-        // trig = event sender
-        var trig = $(this);
-        // toggle the next available element of .content-body-wrapper near the "trig" with duration "fast"
-        if (trig.parent('.collapsible').length !== 0) {
-            trig.parent().children('.content-body-wrapper, .content-footer').slideToggle('fast');
-            trig.toggleClass( 'inactive',  !trig.hasClass('inactive') );
-        }
-    });
 
     // set mouse curser on mouse-over to pointer
     $('.collapsible').children('.content-header').css('cursor','pointer');
+
+    // map click events
+    $('.collapsible').children('.content-header').on("click",collapseElement);
 });
