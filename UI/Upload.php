f7360509efe6803d650012e31719916dd1076926
Florian Lücke <florian.luecke@gmail.com>
2013-12-31 12:39:56 +0100

    renames EerciseUpload.template.html to Upload.template.html

--- UI/Upload.php
+++ UI/Upload.php
@@ -15,7 +15,7 @@ $h->setBackURL('index.php');
  */
 $h->setPoints(75);
 
-$t = Template::WithTemplateFile('include/Upload/EerciseUpload.template.html');
+$t = Template::WithTemplateFile('include/Upload/Upload.template.html');
 
 $w = new HTMLWrapper($h, $t);
 $w->set_config_file('include/configs/config_upload_exercise.json');
