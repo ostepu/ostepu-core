b738b4d5bf003e2e61ad8ba7328d962c2df59605
Florian Lücke <florian.luecke@gmail.com>
2014-01-01 22:35:09 +0100

    adds form evaluation for TutorAssign page

--- UI/TutorAssign.php
+++ UI/TutorAssign.php
@@ -2,7 +2,18 @@
 include 'include/Header/Header.php';
 include 'include/HTMLWrapper.php';
 include_once 'include/Template.php';
+?>
 
+<?php
+    if (isset($_POST['action'])) {
+        Logger::Log($_POST, LogLevel::INFO);
+        header("Location: TutorAssign.php");
+    } else {
+        Logger::Log("No Assignment Data", LogLevel::INFO);
+    }
+?>
+
+<?php
 // construct a new Header
 $h = new Header("Datenstrukturen",
                 "",
@@ -10,7 +21,7 @@ $h = new Header("Datenstrukturen",
                 "211221492");
 
 $h->setBackURL("index.php")
-->setBackTitle("zur Veranstaltung");
+  ->setBackTitle("zur Veranstaltung");
 
 $data = file_get_contents("http://localhost/Uebungsplattform/UI/Data/TutorAssignData");
 $data = json_decode($data, true);
@@ -26,7 +37,7 @@ $assignManually = Template::WithTemplateFile('include/TutorAssign/AssignManually
 $assignManually->bind($tutorAssignment);
 
 // construct a content element for joining groups
-$assignCancel = Template::WithTemplateFile('include/TutorAssign/AssignCancel.template.html');
+$assignCancel = Template::WithTemplateFile('include/TutorAssign/AssignRemove.template.html');
 
 // wrap all the elements in some HTML and show them on the page
 $w = new HTMLWrapper($h, $assignAutomatically, $assignManually, $assignCancel);
