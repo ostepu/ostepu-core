6477a0375567ca00a2015dbf1f422253d7d7f657
Florian Lücke <florian.luecke@gmail.com>
2014-01-08 22:39:43 +0100

    data is now loaded from the database

--- UI/Student.php
+++ UI/Student.php
@@ -2,24 +2,41 @@
 include_once 'include/Header/Header.php';
 include_once 'include/HTMLWrapper.php';
 include_once 'include/Template.php';
+include_once 'include/Helpers.php';
+
+if (isset($_GET['cid'])) {
+    $cid = $_GET['cid'];
+} else {
+    die('no course id!\n');
+}
+
+if (isset($_GET['uid'])) {
+    $uid = $_GET['uid'];
+} else {
+    die('no user id!\n');
+}
 
 // construct a new header
 $h = new Header("Datenstrukturen",
                 "",
                 "Florian Lücke",
                 "211221492");
-$h->setBackURL('index.php');
+$h->setBackURL("index.php?uid={$uid}");
 
 /*
  * if (is_student($user))
  */
 $h->setPoints(75);
 
+$databaseURL = "http://141.48.9.92/uebungsplattform/DB/DBExerciseSheet/exercisesheet/course/{$cid}/exercise";
+
 // construct some exercise sheets
-$sheetString = file_get_contents("http://localhost/Uebungsplattform/UI/Data/SheetData");
+$sheetString = http_get($databaseURL);
 
 // convert the json string into an associative array
-$sheets = json_decode($sheetString, true);
+$sheets = array("sheets" =>json_decode($sheetString, true),
+                "uid" => $uid,
+                "cid" => $cid);
 
 $t = Template::WithTemplateFile('include/ExerciseSheet/ExerciseSheetStudent.template.html');
 
