763d37c85c45e71014e3dc3b480ed515d06d8a2d
Florian Lücke <florian.luecke@gmail.com>
2014-01-12 21:11:06 +0100

    make use of the new MakeNotification function

--- UI/Lecturer.php
+++ UI/Lecturer.php
@@ -3,16 +3,20 @@ include_once 'include/HTMLWrapper.php';
 include_once 'include/Template.php';
 include_once 'include/Helpers.php';
 
+$notifications = array();
+
 if (isset($_GET['cid'])) {
     $cid = $_GET['cid'];
 } else {
-    die('no course id!\n');
+    $uid = -1;
+    $notifications[] = MakeNotification("error", "No course id!");
 }
 
 if (isset($_GET['uid'])) {
     $uid = $_GET['uid'];
 } else {
-    die('no user id!\n');
+    $cid = -1;
+    $notifications[] = MakeNotification("error", "No user id!");
 }
 
 // load user data from the database
@@ -20,10 +24,18 @@ $databaseURI = "http://141.48.9.92/uebungsplattform/DB/DBControl/user/user/{$uid
 $user = http_get($databaseURI);
 $user = json_decode($user, true);
 
+if (is_null($user)) {
+    $user = array();
+}
+
 // load course data from the database
 $databaseURI = "http://141.48.9.92/uebungsplattform/DB/DBControl/course/course/{$cid}";
 $course = http_get($databaseURI);
 $course = json_decode($course, true)[0];
+
+if (is_null($course)) {
+    $course = array();
+}
 
 $menu = Template::WithTemplateFile('include/Navigation/NavigationLecturer.template.html');
 
@@ -33,9 +45,7 @@ $h->bind($user);
 $h->bind($course);
 $h->bind(array("backTitle" => "Veranstaltung wechseln",
                "navigationElement" => $menu,
-               "notificationElements" => array('<div class="notification-bar success">Mimimi</div>',
-                                               '<div class="notification-bar warning">Mimimi</div>',
-                                               '<div class="notification-bar error">Mimimi</div>')));
+               "notificationElements" => $notifications));
 
 $databaseURL = "http://141.48.9.92/uebungsplattform/DB/DBExerciseSheet/exercisesheet/course/{$cid}/exercise";
 
