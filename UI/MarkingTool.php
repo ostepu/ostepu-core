68eab6cbf0d54f765e245912e49f02a7f395fd49
Felix Schmidt <Fiduz@Live.de>
2014-01-12 22:33:41 +0100

    uses new Header in MarkingTool.php

--- UI/MarkingTool.php
+++ UI/MarkingTool.php
@@ -1,16 +1,40 @@
 <?php
-include 'include/Header/Header.php';
 include 'include/HTMLWrapper.php';
 include_once 'include/Template.php';
 
-// construct a new Header
-$h = new Header("Datenstrukturen",
-                "",
-                "Florian Lücke",
-                "211221492");
+$notifications = array();
 
-$h->setBackURL("index.php")
-->setBackTitle("zur Veranstaltung");
+if (isset($_GET['uid'])) {
+    $uid = $_GET['uid'];
+} else {
+    $uid = 0;
+}
+
+if (isset($_GET['cid'])) {
+    $cid = $_GET['cid'];
+} else {
+    die('no course id!\n');
+}
+
+// load user data from the database
+$databaseURI = "http://141.48.9.92/uebungsplattform/DB/DBControl/user/user/{$uid}";
+$user = http_get($databaseURI);
+$user = json_decode($user, true);
+
+// load course data from the database
+$databaseURI = "http://141.48.9.92/uebungsplattform/DB/DBControl/course/course/{$cid}";
+$course = http_get($databaseURI);
+$course = json_decode($course, true)[0];
+
+
+// construct a new header
+$h = Template::WithTemplateFile('include/Header/Header.template.html');
+$h->bind($user);
+$h->bind($course);
+$h->bind(array("backTitle" => "zur Veranstaltung",
+               "backURL" => "Tutor.php?cid={$cid}&uid={$uid}",
+               "notificationElements" => $notifications));
+
 
 $searchSettings = Template::WithTemplateFile('include/MarkingTool/SearchSettings.template.html');
 $markingElement = Template::WithTemplateFile('include/MarkingTool/MarkingElement.template.html');
