1c30ffd2f50ba749d2ffa862abf834f997050993
Felix Schmidt <Fiduz@Live.de>
2014-01-12 22:36:01 +0100

    uses new Header in RightsManagement.php

--- UI/RightsManagement.php
+++ UI/RightsManagement.php
@@ -1,5 +1,4 @@
 <?php
-include 'include/Header/Header.php';
 include 'include/HTMLWrapper.php';
 include_once 'include/Template.php';
 ?>
@@ -13,14 +12,38 @@ include_once 'include/Template.php';
 ?>
 
 <?php
-// construct a new Header
-$h = new Header("Datenstrukturen",
-                "",
-                "Florian Lücke",
-                "Admin");
+if (isset($_GET['cid'])) {
+    $cid = $_GET['cid'];
+} else {
+    die('no course id!\n');
+}
 
-$h->setBackURL("index.php")
-->setBackTitle("zur Veranstaltung");
+if (isset($_GET['uid'])) {
+    $uid = $_GET['uid'];
+} else {
+    die('no user id!\n');
+}
+
+// load user data from the database
+$databaseURI = "http://141.48.9.92/uebungsplattform/DB/DBControl/user/user/{$uid}";
+$user = http_get($databaseURI);
+$user = json_decode($user, true);
+
+// load course data from the database
+$databaseURI = "http://141.48.9.92/uebungsplattform/DB/DBControl/course/course/{$cid}";
+$course = http_get($databaseURI);
+$course = json_decode($course, true)[0];
+
+$menu = Template::WithTemplateFile('include/Navigation/NavigationAdmin.template.html');
+
+// construct a new header
+$h = Template::WithTemplateFile('include/Header/Header.template.html');
+$h->bind($user);
+$h->bind($course);
+$h->bind(array("backTitle" => "zur Veranstaltung",
+               "backURL" => "Admin.php?cid={$cid}&uid={$uid}",
+               "navigationElement" => $menu,
+               "notificationElements" => $notifications));
 
 // construct a content element for setting tutor rights
 $tutorRights = Template::WithTemplateFile('include/RightsManagement/TutorRights.template.html');
