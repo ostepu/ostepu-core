d764c2555f81a6b8a3bebdc7a654c69cecb253c1
Florian Lücke <florian.luecke@gmail.com>
2014-01-14 19:30:06 +0100

    adds some more documentation

--- logic/User/User.php
+++ logic/User/User.php
@@ -3,42 +3,59 @@
 require 'Slim/Slim.php';
 include 'include/Request.php';
 include_once( 'include/CConfig.php' );
-    
+
 \Slim\Slim::registerAutoloader();
 
 class LUser
 {
     private $_conf=null;
-    
+
+    /**
+     * Prefix of this component
+     */
     private static $_prefix = "user";
-    
+
+    /**
+     * Get the Prefix of this component.
+     *
+     * @return mixed
+     */
     public static function getPrefix()
     {
         return LUser::$_prefix;
     }
+
+    /**
+     * Sets the Prefix of this component.
+     *
+     * @param mixed $_prefix the _prefix
+     *
+     * @return self
+     */
     public static function setPrefix($value)
     {
         LUser::$_prefix = $value;
     }
+
     //the URL of the Logic-Controller
     private $lURL = "";             //Einlesen aus config
 
     /**
      * REST actions
      *
-     * This function contains the REST actions with the assignments to 
+     * This function contains the REST actions with the assignments to
      * the functions.
      */
     public function __construct($conf)
-    {    
+    {
         $this->app = new \Slim\Slim();
         $this->app->response->headers->set('Content-Type', 'application/json');
         $this->_conf = $conf;
         $this->query = array();
-        
+
         $this->query = CConfig::getLink($conf->getLinks(),"controller");
         $this->lURL = $this->query->getAddress();
-    
+
         //SetUserRights
         $this->app->put('/user/:userid/right', array($this, 'setUserRights'));          //Adressen noch anpassen(kein .php;+ Compo-Namen
 
@@ -55,29 +72,32 @@ class LUser
         $this->app->get('/user/:userid', array($this, 'getUser'));
 
         $this->app->run();
-    }        
+    }
+
     /**
-     * set the user rights
-     * 
-     * This function changes the right of the user.
+     * Set a user's uer-rights.
      *
-     * @return integer $status the status code
+     * Called when this component receives an HTTP POST request to
+     * /users/$userid/rights(/).
+     * The request body should contain a JSON object representing the user's new
+     * rights.
+     *
+     * @param int $userid The id of the user whose rights should be updated.
      */
-    public function setUserRights($userid){        
+    public function setUserRights($userid){
         $body = $this->app->request->getBody();
         $header = $this->app->request->headers->all();
         $URL = $this->lURL.'/DB/user/user/'.$userid.'/right';
         $answer = Request::custom('PUT', $URL, $header, $body);
-        $this->app->response->setStatus($answer['status']);       
+        $this->app->response->setStatus($answer['status']);
     }
-    
+
     /**
-     * add a new user
+     * Adds a new user.
      *
-     * This function adds a user.
-     * The new user will be written in the database.
-     *
-     * @return integer $status the status code
+     * Called when this component reveives an HTTP POST request to
+     * /users(/).
+     * The request body should contain a JSON object representing the new user.
      */
     public function addUser($userid){
         //Parameter abfangen wenn $data "nicht leer"
@@ -85,59 +105,66 @@ class LUser
         $header = $this->app->request->headers->all();
         $URL = $this->lURL.'/DB/user';
         $answer = Request::custom('POST', $URL, $header, $body);
-        $this->app->response->setStatus($answer['status']);       
+        $this->app->response->setStatus($answer['status']);
     }
 
     /**
-     * edit a user
+     * Edits a user.
      *
-     * This function edit a user.
-     * The user will be overwritten in the database.
+     * Called when this component receives an HTTP PUT request to
+     * /users/$userid(/).
+     * The request body should contain a JSON object representing the user's new
+     * attributes
+     *
+     * @param int $userid The id of the user that is beeing updated.
      */
-    public function editUser($userid){        
+    public function editUser($userid){
         $body = $this->app->request->getBody();
         $header = $this->app->request->headers->all();
         $URL = $this->lURL.'/DB/user/course/'.$userid;
         $answer = Request::custom('PUT', $URL, $header, $body);
-        $this->app->response->setStatus($answer['status']);        
+        $this->app->response->setStatus($answer['status']);
     }
-        
-     /**
-     * return a list of all users
-     * 
-     * This function returns a list of all users from the database.
+
+    /**
+     * Returns a list of all users.
+     *
+     * Called when this component receives an HTTP GET request to /users(/).
      */
-    public function getUsers(){        
+    public function getUsers(){
         $body = $this->app->request->getBody();
         $header = $this->app->request->headers->all();
         $URL = $this->lURL.'/DB/user/user';
         $answer = Request::custom('GET', $URL, $header, $body);
-        $this->app->response->setStatus($answer['status']);                
+        $this->app->response->setStatus($answer['status']);
         $this->app->response->setBody($answer['content']);
     }
-        
-     /**
-     * get a user
-     * 
-     * This function returns a user from the database.
+
+    /**
+     * Returns a user.
+     *
+     * Called when this component receives an HTTP GET request to
+     * /users/$userid(/).
+     *
+     * @param int $userid The id of the user that should be returned.
      */
-    public function getUser($userid){        
+    public function getUser($userid){
         $body = $this->app->request->getBody();
         $header = $this->app->request->headers->all();
         $URL = $this->lURL.'/DB/user/user/'.$userid;
-        $answer = Request::custom('GET', $URL, $header, $body);            
-        $this->app->response->setStatus($answer['status']);                
-        $this->app->response->setBody($answer['content']);    
+        $answer = Request::custom('GET', $URL, $header, $body);
+        $this->app->response->setStatus($answer['status']);
+        $this->app->response->setBody($answer['content']);
     }
 }
 
 /**
- * get new Config-Datas from DB 
+ * get new config data from DB
  */
 $com = new CConfig(LUser::getPrefix());
 
 /**
- * make a new instance of User-Class with the Config-Datas
+ * create a new instance of LUser class with the config data
  */
 if (!$com->used())
     new LUser($com->loadConfig());
