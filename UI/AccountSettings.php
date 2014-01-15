4372f2cca5bbb8cb738e87300e4f55217e99914b
Florian Lücke <florian.luecke@gmail.com>
2014-01-15 18:02:59 +0100

    fixes AccoutSettings page

--- UI/AccountSettings.php
+++ UI/AccountSettings.php
@@ -1,5 +1,4 @@
 <?php
-include 'include/Header/Header.php';
 include 'include/HTMLWrapper.php';
 include_once 'include/Template.php';
 
@@ -11,31 +10,17 @@ if (isset($_GET['uid'])) {
     $uid = 0;
 }
 
-if (isset($_GET['cid'])) {
-    $cid = $_GET['cid'];
-} else {
-    die('no course id!\n');
-}
-
 // load user data from the database
 $databaseURI = "http://141.48.9.92/uebungsplattform/DB/DBControl/user/user/{$uid}";
 $user = http_get($databaseURI);
 $user = json_decode($user, true);
 
-// load course data from the database
-$databaseURI = "http://141.48.9.92/uebungsplattform/DB/DBControl/course/course/{$cid}";
-$course = http_get($databaseURI);
-$course = json_decode($course, true)[0];
-
-$menu = Template::WithTemplateFile('include/Navigation/NavigationAdmin.template.html');
-
 // construct a new header
 $h = Template::WithTemplateFile('include/Header/Header.template.html');
 $h->bind($user);
-$h->bind($course);
-$h->bind(array("backTitle" => "zur Veranstaltung",
-               "backURL" => "Admin.php?cid={$cid}&uid={$uid}",
-               "navigationElement" => $menu,
+$h->bind(array("backTitle" => "Veranstaltungen",
+               "name" => "Accounteinstellungen",
+               "backURL" => "index.php?uid={$uid}",
                "notificationElements" => $notifications));
 
 
