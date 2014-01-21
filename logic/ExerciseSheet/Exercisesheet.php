878e1a25a4e87dda7fc644a68e5a2a3f0a30ce9d
Peter König <upbkgs20@arcor.de>
2014-01-21 11:07:09 +0100

    Little fixes in the Condition documentation and completion of the ExerciseSheet functions.

--- logic/ExerciseSheet/Exercisesheet.php
+++ logic/ExerciseSheet/Exercisesheet.php
@@ -9,12 +9,12 @@ include_once( 'include/CConfig.php' );
 /**
  * The ExerciseSheet class
  *
- * This class handles everything belongs to an ExerciseSheet
+ * This class handles everything belongs to an ExerciseSheet.
  */
 class LExerciseSheet
 {
     /**
-     * Values that are required for communication with other components
+     * Values that are required for communication with other components.
      */
     private $_conf=null;
     private static $_prefix = "exercisesheet";
@@ -30,20 +30,20 @@ class LExerciseSheet
 
     /**
      * Address of the Logic-Controller
-     * dynamic set by CConf below
+     * dynamic set by CConf below.
      */
     private $lURL = "";
 
     public function __construct($conf)
     {
         /**
-         * Initialise the Slim-Framework
+         * Initialise the Slim-Framework.
          */
         $this->app = new \Slim\Slim();
         $this->app->response->headers->set('Content-Type', 'application/json');
         /**
          * Get the URL of the Logic-Controller of the CConf.json file and set
-         * the $lURL variable
+         * the $lURL variable.
          */
         $this->_conf = $conf;
         $this->query = array();
@@ -53,86 +53,133 @@ class LExerciseSheet
         /**
          * When getting a POST
          * and there are the parameters "/course/1" for example,
-         * the addExerciseSheet function is called
+         * the addExerciseSheet function is called.
          */
-        $this->app->post('/'.$this->_prefix.'/course/:courseid(/)', array($this, 'addExerciseSheet'));        //Adressen noch anpassen (Parameter mit Compo-Namen)
+        $this->app->post('/'.$this->_prefix.'/course/:courseid(/)', array($this, 'addExerciseSheet'));
 
         /**
          * When getting a PUT
          * and there are the parameters "/exercisesheet/1" for example,
-         * the aditExerciseSheet function is called
+         * the aditExerciseSheet function is called.
          */
-        $this->app->put('/'.$this->getPrefix().'/exercisesheet/:sheetid(/)',
+        $this->app->put('/'.$this->_prefix.'/exercisesheet/:sheetid(/)',
                         array ($this, 'editExerciseSheet'));
 
         /**
          * When getting a GET 
          * and there are the parameters "/exercisesheet/1/url" for example,
-         * the getExerciseSheetURL function is called
+         * the getExerciseSheetURL function is called.
          */
-        $this->app->get('/'.$this->getPrefix().'/exercisesheet/:sheetid/url(/)',
+        $this->app->get('/'.$this->_prefix.'/exercisesheet/:sheetid/url(/)',
                         array($this, 'getExerciseSheetURL'));
 
         /**
          * When getting a GET
          * and there are the parameters "/exercisesheet/1" for example,
-         * the getExerciseSheet function is called
+         * the getExerciseSheet function is called.
          */
-        $this->app->get('/'.$this->getPrefix().'/exercisesheet/:sheetid(/)',
+        $this->app->get('/'.$this->_prefix.'/exercisesheet/:sheetid(/)',
                         array($this, 'getExerciseSheet'));
 
         /**
          * When getting a DELETE
          * and there are the parameters "/exercisesheet/1" for example,
-         * the deleteExerciseSheet function is called
+         * the deleteExerciseSheet function is called.
          */
-        $this->app->delete('/'.$this->getPrefix().'/exercisesheet/:sheetid(/)',
+        $this->app->delete('/'.$this->_prefix.'/exercisesheet/:sheetid(/)',
                         array($this, 'deleteExerciseSheet'));
 
         /**
-         * runs the application
+         * Runs the application.
          */
         $this->app->run();
     }
 
-    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Zwei Files im Body
-    public function addExerciseSheet($courseid){
+    /**
+     * Prerequisite: Files in body.
+     */
+     public function addExerciseSheet($courseid){
         $header = $this->app->request->headers->all();
         $body = json_decode($this->app->request->getBody());
-        $samplesolutionfile = json_encode($body->{'_sampleSolution'});      //SampleSolutionfile; mit oder ohne "_"?
-        $sheetfile = json_encode($body->{'_sheetFile'});      //SheetFile; mit oder ohne "_"?
-        //Anfrage an FileSystem
-        $URL = $this->lURL.'/FS';
-        $sampleanswer = Request::custom('POST', $URL, $header, $samplesolutionfile);
-        $sheetanswer = Request::custom('POST', $URL, $header, $sheetfile);
+        $samplesolutionfile = json_encode($body->{'sampleSolution'});
+        $sheetfile = json_encode($body->{'sheetFile'});
+        $attachments = json_encode($body->{'attachments'});
+        /**
+         *Request to file system.
+         */
+        $sampleURL = $this->lURL.'/FS/samplesolution/course/'.$courseid;
+        $sheetURL = $this->lURL.'/FS/sheetfile/course/'.$courseid;
+        $attachmentsURL = $this->lURL.'/FS/attachments/course/'.$courseid;
+        $sampleanswer = Request::custom('POST', $sampleURL, $header, $samplesolutionfile);
+        $sheetanswer = Request::custom('POST', $sheetURL, $header, $sheetfile);
+        $attachmentsanswer = Request::custom('POST', $attachmentsURL, $header, $attachments);
 
-        if($sampleanswer['status'] == 200 and $sheetanswer['status'] == 200){ //nur, wenn Files tatsächlich im FS gespeichert wurden
-            $body->{'_file'} = $answer['content'];      //hier zwei Files
-            //Anfrage an DataBase
-            $URL = $this->lURL.'/DB';
+        /**
+         * If the files saved in the file system... .
+         */
+        if($sampleanswer['status'] == 200 and $sheetanswer['status'] == 200
+        and $attachmentsanswer['status'] == 200){
+            $body->{'sampleSolution'} = $sampleanswer['content'];
+            $body->{'sheetFile'} = $sheetanswer['content'];
+            $body->{'attachments'} = $attachmentsanswer['content'];
+            /**
+            *Request to database.
+            */
+            $URL = $this->lURL.'/DB/course/'.$courseid;
             $answer = Request::custom('POST', $URL, $header, json_encode($body));
             $this->app->response->setStatus($answer['status']);
         }
     }
 
-    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Zwei Files im Body
+    /**
+     * Prerequisite: Files in body.
+     */
     public function editExerciseSheet($sheetid){
-    
+        $header = $this->app->request->headers->all();
+        $body = json_decode($this->app->request->getBody());
+        $samplesolutionfile = json_encode($body->{'sampleSolution'});
+        $sheetfile = json_encode($body->{'sheetFile'});
+        $attachments = json_encode($body->{'attachments'});
+        /**
+         *Request to file system.
+         */
+        $sampleURL = $this->lURL.'/FS/samplesolution/exercisesheet/'.$sheetid;
+        $sheetURL = $this->lURL.'/FS/sheetfile/course/exercisesheet/'.$sheetid;
+        $attachmentsURL = $this->lURL.'/FS/attachments/exercisesheet/'.$sheetid;
+        $sampleanswer = Request::custom('POST', $sampleURL, $header, $samplesolutionfile);
+        $sheetanswer = Request::custom('POST', $sheetURL, $header, $sheetfile);
+        $attachmentsanswer = Request::custom('POST', $attachmentsURL, $header, $attachments);
+
+        /**
+         * If the files saved in the file system... .
+         */
+        if($sampleanswer['status'] == 200 and $sheetanswer['status'] == 200
+        and $attachmentsanswer['status'] == 200){
+            $body->{'sampleSolution'} = $sampleanswer['content'];
+            $body->{'sheetFile'} = $sheetanswer['content'];
+            $body->{'attachments'} = $attachmentsanswer['content'];
+            /**
+            *Request to database.
+            */
+            $URL = $this->lURL.'/DB/exercisesheet/'.$sheetid;
+            $answer = Request::custom('POST', $URL, $header, json_encode($body));
+            $this->app->response->setStatus($answer['status']);
+        }
     }
 
     public function getExerciseSheetURL($sheetid){
         $header = $this->app->request->headers->all();
         $body = $this->app->request->getBody();
-        $URL = $this->lURL.'/DB/exercisesheet/'.$exercisesheetid.'/url';
+        $URL = $this->lURL.'/DB/exercisesheet/'.$sheetid.'/url';
         $answer = Request::custom('GET', $URL, $header, $body);
         $this->app->response->setBody($answer['content']);
         $this->app->response->setStatus($answer['status']);
     }
 
-    public function getExerciseSheet($sheetid, $userid){
+    public function getExerciseSheet($sheetid){
         $header = $this->app->request->headers->all();
         $body = $this->app->request->getBody();
-        $URL = $this->lURL.'/DB/exerciseSheet/'.$sheetid;
+        $URL = $this->lURL.'/DB/exercisesheet/'.$sheetid;
         $answer = Request::custom('GET', $URL, $header, $body);
         $this->app->response->setBody($answer['content']);
         $this->app->response->setStatus($answer['status']);
@@ -142,15 +189,18 @@ class LExerciseSheet
         $header = $this->app->request->headers->all();
         $body = $this->app->request->getBody();
         $URL = $this->lURL.'/DB/exercisesheet/'.$sheetid;
-        $answer = Request::custum('DELETE', $URL, $header, $body);
+        $answer = Request::custom('DELETE', $URL, $header, $body);
         $this->app->response->setStatus($answer['status']);
 
         /**
-         * if DB-Request was succsessfull the file also gets removed from FS 
-         * otherwise returns the Status-Code from DB 
+         * If the request of the database was successful the file also gets
+         * removed from file system otherwise returns the Status-Code from 
+         * database.
          */
         $fileObject = json_decode($answer['content']);
-        //if address-field exists, read it out
+        /**
+         * If address-field exists, read it out.
+         */
         if (isset($fileObject->{'address'})){
             $fileAddress = $fileObject->{'address'};
         }
@@ -161,12 +211,12 @@ class LExerciseSheet
     }
 }
 /**
- * get new Config-Datas from DB
+ * Get new Config-Datas from database.
  */
 $com = new CConfig(LExerciseSheet::getPrefix());
 
 /**
- * make a new instance of ExerciseSheet-Class with the Config-Datas
+ * Make a new instance of ExerciseSheet-class with the config-datas.
  */
 if (!$com->used())
     new LExerciseSheet($com->loadConfig());
