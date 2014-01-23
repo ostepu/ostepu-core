b2f12cb7a0c42158cfe175781eabfc376e82cdcd
Lisa Dietrich <Lisa.Dietrich@student.uni-halle.de>
2014-01-23 13:11:51 +0100

    some Triggers fixed, added, proofed

--- DB/DBSession/Sql/AddSession.sql
+++ DB/DBSession/Sql/AddSession.sql
@@ -1,9 +1,14 @@
 /**
  * @file AddSession.sql
  * inserts a session into %Session table
- * @author  Till Uhlig
+ * @author  Till Uhlig, edited by Lisa Dietrich
  * @param string \$values the input data, e.g. 'a=1, b=2'
  * @result -
  */
  
-INSERT INTO `Session` SET $values
+/*INSERT INTO session (U_id, SE_sessionID) 
+VALUES ($userid, $sessionid)
+ON DUPLICATE KEY UPDATE SE_sessionID = $sessionid*/
+
+INSERT INTO `SESSION` (U_id, SE_sessionID)
+VALUES $values
