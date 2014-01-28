2f9f021aacc127ec3bea0052a1506a1ff9ec72fc
Till Uhlig <till.uhlig@student.uni-halle.de>
2014-01-28 22:22:37 +0100

    - AddSession.sql fixed

--- DB/DBSession/Sql/AddSession.sql
+++ DB/DBSession/Sql/AddSession.sql
@@ -6,6 +6,6 @@
  * @result -
  */
  
-INSERT INTO session (U_id, SE_sessionID) 
+INSERT INTO Session (U_id, SE_sessionID) 
 VALUES ($userid, '$sessionid')
 ON DUPLICATE KEY UPDATE SE_sessionID = '$sessionid'
