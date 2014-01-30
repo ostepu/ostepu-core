075d533003fc10932cecccac41dabd87621b114b
Martin Daute <martin.daute@student.uni-halle.de>
2014-01-31 00:39:26 +0100

    added documentation to LController.php; improved the syntax of LController.php

--- logic/Controller/LController.php
+++ logic/Controller/LController.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @file LController.php Contains the LController class
+ * 
+ * @author Martin Daute
+ * @author Christian Elze
+ * @author Peter Koenig
+ */
 
 require 'Slim/Slim.php';
 include 'include/Request.php';
@@ -6,101 +13,137 @@ include_once( 'include/CConfig.php' );
 
 \Slim\Slim::registerAutoloader();
 
+/**
+ * A class, to send all requests from userinterface
+ * on the correct logic component.
+ */
 class LController
 {
     /**
-     *Values needed for conversation with other components
+     * @var Component $_conf the component data object
      */
     private $_conf=null;
 
+    /**
+     * @var string $_prefix the prefix, the class works with
+     */
     private static $_prefix = "";
 
+    /**
+     * the $_prefix getter
+     *
+     * @return the value of $_prefix
+     */
     public static function getPrefix()
     {
         return LController::$_prefix;
     }
+
+    /**
+     * the $_prefix setter
+     *
+     * @param string $value the new value for $_prefix
+     */
     public static function setPrefix($value)
     {
         LController::$_prefix = $value;
     }
 
+    /**
+     * REST actions
+     *
+     * This function contains the REST actions with the assignments to
+     * the functions.
+     *
+     * @param Component $conf component data
+     */
     public function __construct($conf)
     {
+        // initialize slim    
         $this->app = new \Slim\Slim();
         $this->app->response->headers->set('Content-Type', 'application/json');
 
+        // initialize component
         $this->_conf = $conf;
-        $this->query = array();
-
         $this->query = $conf->getLinks();
 
+        // POST/GET/PUT/DELETE chooseDestination
         $this->app->map('/:string+', array($this, 'chooseDestination'))
                     ->via('POST', 'GET', 'PUT', 'DELETE');
 
+        // run Slim
         $this->app->run();
     }
 
     /**
-     * pick out the config.ini file and store all addresses in $links
+     * Chooses the destination for a request.
      *
-     * @param (param)
+     * Called always when this component receives a 
+     * HTTP POST, GET, PUT or DELETE
+     *
+     * @param array $string An array of strings that contains the URL
+     * with which the controller has been called.
      */
-
     public function chooseDestination($string){
         $method = $this->app->request->getMethod();
         $body = $this->app->request->getBody();
         $header = $this->app->request->headers->all();
 
+        // if the prefix is "DB", send the request on the database controller
         if ($string[0] == "DB") {
             unset($string[0]);
-            $URI = CConfig::getLink($this->query, "database")->getAddress();//DB-URL;                                                            //URI ergänzen
-            foreach ($string as $str) {
-                $URI = $URI.'/'.$str;
-            }
-            $answer = Request::custom($method, $URI, $header, $body);
-            $this->app->response->setBody($answer['content']);
+            // set the database URL
+            $URI = CConfig::getLink($this->query, "database")->getAddress();
+            $this->sendNewRequest($string, $method, $URI, $header, $body);
         } elseif ($string[0] == "FS") {
+        // if the prefix is "FS", send the request on the database controller
             unset($string[0]);
-            $URI = CConfig::getLink($this->query, "filesystem")->getAddress();//FS-URL;                                                            //URI ergänzen
-            foreach ($string as $str) {
-                $URI = $URI.'/'.$str;
-            }
-            $answer = Request::custom($method, $URI, $header, $body);
-            $this->app->response->setBody($answer['content']);
+            // set the filesystem URL
+            $URI = CConfig::getLink($this->query, "filesystem")->getAddress();
+            $this->sendNewRequest($string, $method, $URI, $header, $body);
         } else {
-            $URI = $this->getLink($this->query,$string[0]);//L-URL
-            foreach ($string as $str) {
-                $URI = $URI.'/'.$str;
+        // if the prefix is another one, send the request on the corresponding logic component
+            $arrayOfLinks = $this->query;
+            // search for the correct component URL and set it
+            foreach ($arrayOfLinks as $linkObj){
+                if ($linkObj->getPrefix() == $string[0]){
+                    $URI = $linkObj->getAddress();
+                }
             }
-            $answer = Request::custom($method, $URI, $header, $body);
-            $this->app->response->setBody($answer['content']);
-            $this->app->response->setStatus($answer['status']);
+            $this->sendNewRequest($string, $method, $URI, $header, $body);
         }
     }
-    /**
-     * Funktion to select the right Link from a Linkarray by the prefix
-     * Taking two arguments and returning a Link as string
-     * @param $arrayOfLinks an array of Link-Objects
-     * @param $prefix a string identifies the Component you wants to link to
-     */
-    public function getLink($arrayOfLinks, $prefix){
 
-        foreach ($arrayOfLinks as $linkObj){
-            if ($linkObj->getPrefix() == $prefix){
-                return $linkObj->getAddress();
-            }
+    /**
+     * Sends a new request.
+     *
+     * Called allways if the controller receives a request for
+     * database, filesystem or a logic component.
+     * Completes the URL, sends the new request and sets the response.
+     *
+     * @param array $string An array of strings that contains the URL
+     * with which the controller has been called.
+     * @param string $method The method of the received request.
+     * @param string $URI The first part of the new request.
+     * @param array $header The header of the received request.
+     * @param string $body The body of the received request.
+     */
+    public function sendNewRequest($string, $method, $URI, $header, $body){
+        // completes the URL by attach each field of $string to the given URL-part
+        foreach ($string as $str) {
+            $URI = $URI.'/'.$str;
         }
+        // send the new request and set the response
+        $answer = Request::custom($method, $URI, $header, $body);
+        $this->app->response->setBody($answer['content']);
+        $this->app->response->setStatus($answer['status']);  
     }
 }
 
-/**
- * get new Config-Datas from DB
- */
+// get new config data from DB
 $com = new CConfig(LController::getPrefix());
 
-/**
- * make a new instance of Course-Class with the Config-Datas
- */
+// create a new instance of LUser class with the config data
 if (!$com->used())
     new LController($com->loadConfig());
 ?>
