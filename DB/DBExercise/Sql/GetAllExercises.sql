fd6502716c1306e07965231a0beb3e3aa6aa62ac
Till Uhlig <till.uhlig@student.uni-halle.de>
2014-02-08 19:07:15 +0100

    - LeaderID to all exercise sql querys added

--- DB/DBExercise/Sql/GetAllExercises.sql
+++ DB/DBExercise/Sql/GetAllExercises.sql
@@ -29,6 +29,7 @@ select
     S.S_date as S_date2,
     SS.S_id_selected as S_selected2,
     S.S_accepted as S_accepted2,
+    S.S_leaderId as S_leaderId2,
     S.E_id as E_id2
 from
     Exercise E
