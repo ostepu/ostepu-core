5dc2275691205e7de54cb199a94f60b03f3e50bd
Florian Lücke <florian.luecke@gmail.com>
2014-02-11 14:44:30 +0100

    fixes: accountsettings requires a course id

--- UI/AccountSettings.php
+++ UI/AccountSettings.php
@@ -11,21 +11,21 @@
 include_once 'include/Boilerplate.php';
 
 // load user data from the database
-$databaseURI = $getSiteURI . "/accountsettings/user/{$uid}/course/{$cid}";
-$user_course_data = http_get($databaseURI);
-$user_course_data = json_decode($user_course_data, true);
+$databaseURI = $getSiteURI . "/accountsettings/user/{$uid}";
+$user_data = http_get($databaseURI);
+$user_data = json_decode($user_data, true);
 
 // construct a new header
 $h = Template::WithTemplateFile('include/Header/Header.template.html');
-$h->bind($user_course_data);
-$h->bind(array("name" => $user_course_data['courses'][0]['course']['name'],
+$h->bind($user_data);
+$h->bind(array("name" => "Account Einstellungen",
                "backTitle" => "Veranstaltungen",
                "backURL" => "index.php",
                "notificationElements" => $notifications));
 
 // construct a content element for account information
 $accountInfo = Template::WithTemplateFile('include/AccountSettings/AccountInfo.template.html');
-$accountInfo->bind($user_course_data);
+$accountInfo->bind($user_data);
 
 // construct a content element for changing password
 $changePassword = Template::WithTemplateFile('include/AccountSettings/ChangePassword.template.html');
