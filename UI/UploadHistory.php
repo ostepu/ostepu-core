36545ff87a8ade2225dcac453edb8eb69053e718
Felix Schmidt <Fiduz@Live.de>
2014-02-13 16:53:01 +0100

    adds the ability to change the UploadHistory settings

--- UI/UploadHistory.php
+++ UI/UploadHistory.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * @file Upload.php
+ * @file UploadHistory.php
+ * Constructs the page for managing the upload history of a user.
  *
  * @author Felix Schmidt
  * @author Florian Lücke
@@ -9,27 +10,32 @@
 
 include_once 'include/Boilerplate.php';
 
-if (isset($_POST['sheetID'])) {
-    /**
-     * @todo load data for the slected user
-     */
-    Logger::Log($_POST, LogLevel::INFO);
-} else {
-    Logger::Log("No Data", LogLevel::INFO);
+if (isset($_POST['action'])) {
+    if ($_POST['action'] == "ShowUploadHistory") {
+        if (isset($_POST['userID']) && isset($_POST['sheetID'])) {
+            $uploadUserID = cleanInput($_POST['userID']);
+            $sheetID = cleanInput($_POST['sheetID']);
+
+            // loads the upload history of the selected user (uploadUserID) in the 
+            // selected course from GetSite
+            $URL = $getSiteURI . "/uploadhistory/user/{$uid}/course/{$cid}/exercisesheet/{$sheetID}/uploaduser/{$uploadUserID}";
+            $uploadHistory_data = http_get($URL, false);
+            $uploadHistory_data = json_decode($uploadHistory_data, true);
+            $uploadHistory_data['filesystemURI'] = $filesystemURI;
+        }
+    }
 }
 
-// set $sid = 0 if there is no sid in the URL
-if (!isset($_GET['sid'])) {
-    $sid = 0;
-}
+// loads data for the settings element
+$URL = $getSiteURI . "/uploadhistoryoptions/user/{$uid}/course/{$cid}";
+$uploadHistoryOptions_data = http_get($URL, false);
+$uploadHistoryOptions_data = json_decode($uploadHistoryOptions_data, true);
 
-// load uploadHistory data from GetSite
-$URL = $getSiteURI . "/uploadhistory/user/{$uid}/course/{$cid}/exercisesheet/{$sid}";
-$uploadHistory_data = http_get($URL, false);
-$uploadHistory_data = json_decode($uploadHistory_data, true);
-$uploadHistory_data['filesystemURI'] = $filesystemURI;
+// adds the selected uploadUserID and sheetID
+$uploadHistoryOptions_data['uploadUserID'] = $uploadUserID;
+$uploadHistoryOptions_data['sheetID'] = $sheetID;
 
-$user_course_data = $uploadHistory_data['user'];
+$user_course_data = $uploadHistoryOptions_data['user'];
 
 $menu = MakeNavigationElementForCourseStatus($user_course_data['courses']);
 
@@ -42,18 +48,17 @@ $h->bind(array("name" => $user_course_data['courses'][0]['course']['name'],
                "notificationElements" => $notifications,
                "navigationElement" => $menu));
 
-// construct a content element for the ability to look at the upload history of a student
 
-/**
- * @todo Load uploadHistory data depending on the settings in uploadHistorySettings (student and sheet).
- */
+// construct a content element for the ability to look at the upload history of a student
 $uploadHistorySettings = Template::WithTemplateFile('include/UploadHistory/UploadHistorySettings.template.html');
+$uploadHistorySettings->bind($uploadHistoryOptions_data);
 
 $uploadHistory = Template::WithTemplateFile('include/UploadHistory/UploadHistory.template.html');
 $uploadHistory->bind($uploadHistory_data);
 
 // wrap all the elements in some HTML and show them on the page
 $w = new HTMLWrapper($h, $uploadHistorySettings, $uploadHistory);
+$w->defineForm(basename(__FILE__)."?cid=".$cid, $uploadHistorySettings);
 $w->set_config_file('include/configs/config_default.json');
 $w->show();
 
