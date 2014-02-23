42bb85c90123732506b860b28df4fab392b9ae4b
Felix Schmidt <Fiduz@Live.de>
2014-02-23 23:52:40 +0100

    improvements in Group.php

--- UI/Group.php
+++ UI/Group.php
@@ -26,21 +26,28 @@ $h->bind($user_course_data);
 $h->bind(array("name" => $user_course_data['courses'][0]['course']['name'],
                "notificationElements" => $notifications));
 
+$isInGroup = !empty($group_data['group']['members']);
+$hasInvitations = !empty($group_data['invitations']);
+
 // construct a content element for group information
 $groupMembers = Template::WithTemplateFile('include/Group/GroupMembers.template.html');
 $groupMembers->bind($group_data['group']);
 
 // construct a content element for managing groups
-$manageGroup = Template::WithTemplateFile('include/Group/ManageGroup.template.html');
-$manageGroup->bind($group_data);
+if ($isInGroup) {
+    $manageGroup = Template::WithTemplateFile('include/Group/ManageGroup.template.html');
+    $manageGroup->bind($group_data);
+}
 
 // construct a content element for creating groups
 $createGroup = Template::WithTemplateFile('include/Group/InviteGroup.template.html');
 $createGroup->bind($group_data);
 
 // construct a content element for joining groups
-$invitations = Template::WithTemplateFile('include/Group/Invitations.template.html');
-$invitations->bind($group_data);
+if ($hasInvitations) {
+    $invitations = Template::WithTemplateFile('include/Group/Invitations.template.html');
+    $invitations->bind($group_data);
+}
 
 // wrap all the elements in some HTML and show them on the page
 $w = new HTMLWrapper($h, $groupMembers, $manageGroup, $createGroup, $invitations);
