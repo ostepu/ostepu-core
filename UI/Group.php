6d9a5a2435eeca72838cdd624126ee0ad98de033
Felix Schmidt <Fiduz@Live.de>
2014-02-24 22:36:17 +0100

    adds ability to leave group even as a leader (wow)

--- UI/Group.php
+++ UI/Group.php
@@ -6,47 +6,130 @@
  * @author Felix Schmidt
  * @author Florian Lücke
  * @author Ralf Busch
- *
- * @todo load data from the database and fill the page
  */
 
 include_once 'include/Boilerplate.php';
 
+/**
+ * @todo Create "remove all submissions" function.
+ */
+
+/**
+ * Removes a user from a group.
+ *
+ * @param $uid The id of the user that is being removed
+ * @param $sid The id of the sheet
+ * @param $databaseURI The URI of the database
+ */
+function removeUserFromGroup($uid, $sid)
+{
+    global $databaseURI;
+
+    $newGroupSettings = Group::encodeGroup(Group::createGroup($uid, $uid, $sid));
+    $URI = $databaseURI . "/group/user/{$uid}/exercisesheet/{$sid}";
+    http_put_data($URI, $newGroupSettings, true, $message);
+
+    if ($message == "201") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 if (isset($_POST['action'])) {
-    // removes all group members and deletes the group
+    // removes a group member
     if ($_POST['action'] == "RemoveGroupMember" && isset($_POST['removeMember'])) {
         // extracts the member that is being removed
         $userID = cleanInput($_POST['removeMember']);
 
-        // creates a new group for the user that is being removed
-        $newGroupSettings = Group::encodeGroup(Group::createGroup($userID, $userID, $sid));
-        $URI = $databaseURI . "/group/user/{$userID}/exercisesheet/{$sid}";
-        http_put_data($URI, $newGroupSettings, true, $message);
-
-        if ($message == "201") {
-            $notifications[] = MakeNotification("success", "Der Nutzer {$_POST['kick']} wurde aus der Gruppe entfernt.");
+        // removes the user from the group
+        if (removeUserFromGroup($userID, $sid)) {
+            $notifications[] = MakeNotification("success", "Der Nutzer wurde aus der Gruppe entfernt.");
         } else {
             $notifications[] = MakeNotification("error", "Beim Entfernen ist ein Fehler aufgetreten.");
         }
     }
 
-    // removes a group member
+    // removes an invitation from the group
+    if ($_POST['action'] == "RemoveGroupMember" && isset($_POST['removeInvitation'])) {
+        // extracts the member whose invitation is being removed
+        $userID = cleanInput($_POST['removeInvitation']);
+        
+        // deletes the invitation
+        $URI = $databaseURI . "/invitation/user/{$uid}/exercisesheet/{$sid}/user/{$userID}";
+        http_delete($URI, true, $message);
+
+        if ($message == "201") {
+            $notifications[] = MakeNotification("success", "Die Einladung wurde gelöscht.");
+        } else {
+            $notifications[] = MakeNotification("error", "Beim Löschen der Einladung ist ein Fehler aufgetreten.");
+        }
+    }
+
+    // removes all group members and deletes the group
     if ($_POST['action'] == "RemoveGroupMember" && isset($_POST['leaveGroup'])) {
         // extracts the leader of the group
         $leaderID = cleanInput($_POST['leaveGroup']);
 
-        // check if the user that wants to leave is the leader of the group
+        // checks if the user that wants to leave is the leader of the group
         if ($leaderID == $uid) {
-            /**
-             * @todo Finish this. 
-             */
-        } else {
-            // creates a new group for the user that wants to leave
-            $newGroupSettings = Group::encodeGroup(Group::createGroup($uid, $uid, $sid));
-            $URI = $databaseURI . "/group/user/{$uid}/exercisesheet/{$sid}";
-            http_put_data($URI, $newGroupSettings, true, $message);
+            // bool which is true if any error occured
+            $RequestError = false;
 
-            if ($message == "201") {
+            // returns all invitations from the groups
+            $URI = $databaseURI . "/invitation/leader/exercisesheet/{$sid}/user/{$leaderID}";
+            $invitations = http_get($URI, true);
+            $invitations = json_decode($invitations, true);
+
+            // returns the leader and all members of the group
+            $URI = $databaseURI . "/group/user/{$uid}/exercisesheet/{$sid}";
+            $group = http_get($URI, true);
+            $group = json_decode($group, true);
+
+            // removes all invitations of the group
+            if (!empty($invitations)) {
+                foreach ($invitations as $invitation) {
+                    $URI = $databaseURI . "/invitation/user/{$leaderID}/";
+                    $URI .= "exercisesheet/{$sid}/user/{$invitation['member']['id']}";
+                    
+                    http_delete($URI, true, $message);
+
+                    if ($message != "201") {
+                        $RequestError = true;
+                    }
+                }
+            }
+
+            // removes all users from the group
+            if (!empty($group)) {
+                // removes leader
+                if (!removeUserFromGroup($group['leader']['id'], $sid)) {
+                    $RequestError = true;
+                }
+
+                // removes all members
+                if (!empty($group['members'])) {
+                    foreach ($group['members'] as $member) {
+                        if (!removeUserFromGroup($member['id'], $sid)) {
+                            $RequestError = true;
+                        }
+                    }
+                }
+
+                // shows notification
+                if ($RequestError == false) {
+                    $notifications[] = MakeNotification("success", "Sie haben die Gruppe verlassen.");
+                }
+                else {
+                    $notifications[] = MakeNotification("error", "Beim Verlassen ist ein Fehler aufgetreten!");
+                }
+
+            } else {
+                $notifications[] = MakeNotification("error", "Beim Verlassen ist ein Fehler aufgetreten.");
+            }
+
+        } else {
+            if (removeUserFromGroup($uid, $sid)) {
                 $notifications[] = MakeNotification("success", "Sie haben die Gruppe verlassen.");
             } else {
                 $notifications[] = MakeNotification("error", "Beim Verlassen ist ein Fehler aufgetreten.");
@@ -71,6 +154,10 @@ if (isset($_POST['action'])) {
                 $URI = $databaseURI . "/user/user/{$memberName}";
                 $user_data = http_get($URI, true);
                 $user_data = json_decode($user_data, true);
+
+                /**
+                 * @todo Check if invited user = current user.
+                 */
 
                 // invites the user to the current group
                 if (empty($user_data)) {
@@ -126,9 +213,9 @@ $h->bind($user_course_data);
 $h->bind(array("name" => $user_course_data['courses'][0]['course']['name'],
                "notificationElements" => $notifications));
 
-$isInGroup = !empty($group_data['group']['members']);
+$isInGroup = (!empty($group_data['group']['members']) || !empty($group_data['invitationsFromGroup']));
 $isLeader = $group_data['group']['leader']['id'] == $uid;
-$hasInvitations = !empty($group_data['invitations']);
+$hasInvitations = !empty($group_data['invitationsToGroup']);
 
 $group_data['isInGroup'] = $isInGroup;
 $group_data['isLeader'] = $isLeader;
