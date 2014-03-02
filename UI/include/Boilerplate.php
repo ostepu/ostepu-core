d089dd5454dcf0fe622d27b0a3737472be5e757f
Ralf Busch <ralfbusch92@gmail.com>
2014-03-02 01:46:24 +0100

    deleted double logicURI in Boilerplate

--- UI/include/Boilerplate.php
+++ UI/include/Boilerplate.php
@@ -42,7 +42,6 @@ $serverURI = "http://141.48.9.92/uebungsplattform";
 $databaseURI = $serverURI . "/DB/DBControl";
 $logicURI = $serverURI . "/logic/Controller";
 $filesystemURI = $serverURI . "/FS/FSControl";
-$logicURI = $serverURI . "/logic/Controller";
 $getSiteURI = $serverURI . "/logic/GetSite";
 
 ?>
