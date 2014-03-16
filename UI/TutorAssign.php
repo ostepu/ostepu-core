19f0e3aa1081ba4cfa89f65e641f1416c757107a
Felix Schmidt <Fiduz@Live.de>
2014-03-16 14:45:40 +0100

    adds ability to remove all assignments in TutorAssign.php

--- UI/TutorAssign.php
+++ UI/TutorAssign.php
@@ -10,9 +10,74 @@
 
 include_once 'include/Boilerplate.php';
 
+/**
+ * @todo Use form evaluator.
+ */
+
+if (isset($_POST['action'])) {
+    // automatically assigns all unassigned submissions to the selected tutors
+    if ($_POST['action'] == "AssignAutomatically" && isset($_POST['tutorID'])) {
+        // extracts the php POST data
+        $selectedTutorIDs = cleanInput($_POST['tutorID']);
+
+
+        $data = array('tutors' => array(),
+                      'unassigned' => array());
+
+        // load user data from the database for the first time
+        $URL = $getSiteURI . "/tutorassign/user/{$uid}/course/{$cid}/exercisesheet/{$sid}";
+        $tutorAssign_data = http_get($URL, false);
+        $tutorAssign_data = json_decode($tutorAssign_data, true);
+
+        // adds all tutors that are selected in the form to the request body
+        foreach ($selectedTutorIDs as $tutorID) {
+            $newTutor = array('tutorId' => $tutorID);
+            $data['tutors'][] = $newTutor;
+        }
+
+        // adds all unassigned submissions to the request body
+        if (!empty($tutorAssign_data['tutorAssignments'])) {
+            foreach ($tutorAssign_data['tutorAssignments'] as $tutorAssignment) {
+                if ($tutorAssignment['tutor']['userName'] == "unassigned") {
+                    foreach ($tutorAssignment['submissions'] as $submission) {
+                        $data['unassigned'][] = $submission;
+                    }
+                }
+            }
+        }
+
+        $data = json_encode($data);
+
+        $URI = $logicURI . "/tutor/auto/exercise/course/{$cid}/exercisesheet/{$sid}";
+        http_post_data($URI, $data, true, $message);
+
+        if ($message == "201") {
+            $msg = "Die Zuweisungen wurden erfolgreich geändert.";
+            $notifications[] = MakeNotification("success", $msg);
+        } else {
+            $msg = "Bei der Zuweisung ist ein Fehler aufgetreten.";
+            $notifications[] = MakeNotification("error", $msg);
+        }
+    }
+
+    // removes all tutor assignments by deleting all markings of the exercisesheet
+    if ($_POST['action'] == "AssignRemove") {
+        $URI = $databaseURI . "/marking/exercisesheet/" . $sid;
+        http_delete($URI, true, $message);
+
+        if ($message == "201") {
+            $msg = "Die Zuweisungen wurden erfolgreich aufgehoben.";
+            $notifications[] = MakeNotification("success", $msg);
+        } else {
+            $msg = "Beim Aufheben der Zuweisungen ist ein Fehler aufgetreten.";
+            $notifications[] = MakeNotification("error", $msg);
+        }
+    }
+}
+
 // load user data from the database
 $URL = $getSiteURI . "/tutorassign/user/{$uid}/course/{$cid}/exercisesheet/{$sid}";
-$tutorAssign_data = http_get($URL, true);
+$tutorAssign_data = http_get($URL, false);
 $tutorAssign_data = json_decode($tutorAssign_data, true);
 
 $user_course_data = $tutorAssign_data['user'];
@@ -35,10 +100,12 @@ $assignManually = Template::WithTemplateFile('include/TutorAssign/AssignManually
 $assignManually->bind($tutorAssign_data);
 
 // construct a content element for removing assignments from tutors
-$assignCancel = Template::WithTemplateFile('include/TutorAssign/AssignRemove.template.html');
+$assignRemove = Template::WithTemplateFile('include/TutorAssign/AssignRemove.template.html');
 
 // wrap all the elements in some HTML and show them on the page
-$w = new HTMLWrapper($h, $assignAutomatically, $assignManually, $assignCancel);
+$w = new HTMLWrapper($h, $assignAutomatically, $assignManually, $assignRemove);
+$w->defineForm(basename(__FILE__)."?cid=".$cid."&sid=".$sid, false, $assignAutomatically);
+$w->defineForm(basename(__FILE__)."?cid=".$cid."&sid=".$sid, false, $assignRemove);
 $w->set_config_file('include/configs/config_default.json');
 $w->show();
 
