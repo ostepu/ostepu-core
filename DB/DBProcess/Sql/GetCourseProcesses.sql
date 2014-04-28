b8cda59646df406a181e66bdbe45ef7d69c92cc7
Till Uhlig <till.uhlig@student.uni-halle.de>
2014-04-28 17:17:01 +0200

    Exercise objects to DBProcess querys added

--- DB/DBProcess/Sql/GetCourseProcesses.sql
+++ DB/DBProcess/Sql/GetCourseProcesses.sql
@@ -13,10 +13,18 @@ select
     PRO.E_id,
     PRO.PRO_parameter,
     PRO.CO_id_target,
+    E.ES_id,
+    E.ET_id,
+    E.E_maxPoints,
+    E.E_bonus,
+    E.E_id_link,
+    E.E_linkName,
     CO.CO_id,
     CO.CO_name,
     CO.CO_address
 from
     `Process_{$courseid}` PRO
         left join
-    `Component` CO ON PRO.CO_id_target = CO.CO_id
+    `Component` CO ON (PRO.CO_id_target = CO.CO_id)
+        join
+    `Exercise` E ON (E.E_id = PRO.E_id)
