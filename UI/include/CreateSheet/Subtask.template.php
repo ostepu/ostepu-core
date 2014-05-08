dac64bd753c1aad113fffe85517d5e2340daf589
Till Uhlig <till.uhlig@student.uni-halle.de>
2014-05-08 15:03:15 +0200

    Subtask.template.php, forms and processors if tables are existing

--- UI/include/CreateSheet/Subtask.template.php
+++ UI/include/CreateSheet/Subtask.template.php
@@ -1,27 +1,60 @@
+<?php
+include_once '../../../Assistants/Structures.php';
+include_once '../../../Assistants/Request.php';
+include_once '../Config.php';
+?>
+
 <li>
     <input class="form-field text-input very-short" name="exercises[0][subexercises][0][maxPoints]" placeholder="Punkte" id="exerciseMaxPoints" />
     <select class="form-field text-input short" name="exercises[0][subexercises][0][exerciseType]" id="exerciseType">
         <?php
         session_start();
+        $courseid = null;
         if (isset($_SESSION['JSCACHE'])) {
             $cache = json_decode($_SESSION['JSCACHE'], true);
 
             foreach ($cache as $excercisetype) {
+                $courseid = $excercisetype['courseId'];
                 print "<option value=\"".$excercisetype['exerciseTypeId']."\">".$excercisetype['name']."</option>";
                 print "<option value=\"".$excercisetype['exerciseTypeId']."b\">".$excercisetype['name']." (Bonus)</option>";
             }
         }
+        
+        $forms = false;
+        $processes = false;
+        if ($courseid !== null){
+            $result = Request::get($databaseURI.'/form/exists/course/' . $courseid,array(),'');
+            echo $databaseURI.'/exists/form/course/' . $courseid;
+            if ( $result['status'] === 200 )
+                $forms = true;
+                
+            $result = Request::get($databaseURI.'/process/exists/course/' . $courseid,array(),'');         
+            if ( $result['status'] === 200 )
+                $processes = true;
+        }
+        
         ?>
     </select>
     <input class="form-field text-input very-short mime-field" name="exercises[0][subexercises][0][mime-type]" value="pdf" id="mime-type" placeholder="pdf, zip, html, jpg, gif"/>
     <input class="button" type="file" name="exercises[0][subexercises][0][attachment]" value="Anhang auswählen ..." />
                         <a href="javascript:void(0);" class="body-option-color deny-button delete-subtask right">Teilaufgabe löschen</a>
-        <div class="content-body-wrapper">
+                        
+                        
+                        
+                        
+                    <?php if ($forms || $processes) {?>   
+                    <div class="content-body-wrapper">
                     <table border="0" style="width:100%">
+                    <?php if ($forms){ ?>
                     <tr><td><a href="javascript:void(0);" class="body-option-color very-short use-form">Eingabemaske verwenden</a></td></tr> 
+                    <?php } ?>
                     
-                    <tr><td><a href="javascript:void(0);" class="body-option-color very-short use-processor">Verarbeitung hinzufügen</a></td></tr>                        
+                    <?php if ($processes){ ?>
+                    <tr><td><a href="javascript:void(0);" class="body-option-color very-short use-processor">Verarbeitung hinzufügen</a></td></tr>   
+                    <?php } ?>
+                    
                     </table>
                         </div>
+                    <?php } ?>
     
 </li>
