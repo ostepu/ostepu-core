b79cc8f0a00bdc7004fa73a6cf78c5195c6b98e1
Till Uhlig <till.uhlig@student.uni-halle.de>
2014-05-08 15:06:23 +0200

    LFormPredecessor.template.php, small fixes

--- UI/include/CreateSheet/Processor/LFormPredecessor.template.php
+++ UI/include/CreateSheet/Processor/LFormPredecessor.template.php
@@ -1,3 +1,3 @@
-        <div class="content-element ProcessorParameterArea" style="outline:2px solid #b9b8b8;border-radius: 0px;margin: 0px;">
+        <div class="content-element ProcessorParameterArea" style="solid #b9b8b8;border-radius: 0px;margin: 0px;">
 
             </div>
