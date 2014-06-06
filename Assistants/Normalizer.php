c17363c3373264270ea22a41e3fdeb7e85edb71b
Till Uhlig <till.uhlig@student.uni-halle.de>
2014-06-06 22:45:00 +0200

    Normalizer added

---
+++ Assistants/Normalizer.php
@@ -0,0 +1,11 @@
+<?php
+class Normalizer
+{
+    public static function normalizeText($text){
+        $text = mb_convert_case($text, MB_CASE_LOWER, 'UTF-8');
+        $text = trim($text);
+
+        return utf8_decode($text); 
+    }
+}
+?>
