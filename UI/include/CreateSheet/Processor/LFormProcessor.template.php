63e35d8cbb12d13a5ea5f45de4791e9fd92e5da8
Till Uhlig <till.uhlig@student.uni-halle.de>
2014-06-06 21:28:13 +0200

    - LFormPredecessor template added - template moved from Processor to Predecessor - new LFormProcessor template

--- UI/include/CreateSheet/Processor/LFormProcessor.template.php
+++ UI/include/CreateSheet/Processor/LFormProcessor.template.php
@@ -11,30 +11,23 @@
             <table border="0" style="width:100%">          
                     
        <tr>
-       <td><input type="checkbox" class="parameter-choice" name="exercises[0][subexercises][0][processorParameterList][0][]" value="trim"/></td><td><label class="label bold">trim</label></td>
-       <td><input type="checkbox" class="parameter-choice" name="exercises[0][subexercises][0][processorParameterList][0][]" value="isnumeric"/></td><td><label class="label bold">isnumeric</label></td>
+       
+       <td>
+       <input type="checkbox" class="parameter-choice" name="exercises[0][subexercises][0][processorParameterList][0][]" value="distance1"/>
+       </td>
+       <td>
+       <label class="label bold" for="">Übereinstimmung:</label>
+       </td>
+       <td>
+               <select class="parameter-choice" style="width:auto" name="exercises[0][subexercises][0][processorParameterList][0][]">
+           <option value="25">>=25%</option>
+           <option value="50">>=50%</option>
+           <option value="75">>=75%</option>
+    </select>
+ </td> 
+ 
        </tr>
        
-        <tr>
-        <td><input type="checkbox" class="parameter-choice" name="exercises[0][subexercises][0][processorParameterList][0][]" value="lowerCase"/></td><td><label class="label bold" >lowerCase</label></td>
-        <td><input type="checkbox" class="parameter-choice" name="exercises[0][subexercises][0][processorParameterList][0][]" value="isdigit"/></td><td><label class="label bold">isdigit</label></td>
-        </tr>
-        
-        <tr>
-        <td><input type="checkbox" class="parameter-choice" name="exercises[0][subexercises][0][processorParameterList][0][]" value="upperCase"/></td><td><label class="label bold" >upperCase</label></td>
-        <td><input type="checkbox" class="parameter-choice" name="exercises[0][subexercises][0][processorParameterList][0][]" value="isprintable"/></td><td><label class="label bold">isprintable</label></td>
-        </tr>
-        
-        <tr>
-        <td></td><td></td>
-        <td><input type="checkbox" class="parameter-choice" name="exercises[0][subexercises][0][processorParameterList][0][]" value="isalpha"/></td><td><label class="label bold">isalpha</label></td>
-        </tr>
-        
-        <tr>
-        <td></td><td></td>
-        <td><input type="checkbox" class="parameter-choice" name="exercises[0][subexercises][0][processorParameterList][0][]" value="isalphanum"/></td><td><label class="label bold">isalphanum</label></td>
-        </tr>
-        
         </table>
             </div>
             </div>
