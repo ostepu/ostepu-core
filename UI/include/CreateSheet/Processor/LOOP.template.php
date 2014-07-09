8140606aff3a03475eb9c5c711bc14296f556013
Till Uhlig <till.uhlig@student.uni-halle.de>
2014-07-09 11:22:19 +0200

    attachments disabled belongs to #53

--- UI/include/CreateSheet/Processor/LOOP.template.php
+++ UI/include/CreateSheet/Processor/LOOP.template.php
@@ -10,9 +10,9 @@
             <div class="content-body left" style="width:100%;">
             
             <table border="1" style="width:100%;"> 
-         <tr><td> <label class="short left label bold new-line" for="attachment">Anhang:</label><br><br></td><td></td><td></td></tr>
+            <tr><td> <label class="short left label bold new-line" for="attachment">Anhang:</label><br><br></td><td></td><td></td></tr>
             </table>
-   <a href="javascript:void(0);" class="body-option-color add-attachment right">Anhang hinzufügen</a>
+            <a style="color:#b9b8b8"><!--<a href="javascript:void(0);" class="body-option-color add-attachment right">-->Anhang hinzufügen</a>
  
             </div>
   
