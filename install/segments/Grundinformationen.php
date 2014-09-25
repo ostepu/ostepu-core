9f13cc38430b8e10e03541d559c56a6f27b0c319
Till Uhlig <till.uhlig@student.uni-halle.de>
2014-09-25 19:58:48 +0200

    wrong default temp and files folders fixed

--- install/segments/Grundinformationen.php
+++ install/segments/Grundinformationen.php
@@ -8,16 +8,16 @@ if (!$simple)
         $text .= Design::erstelleZeile($simple, Sprachen::Get('general_informations','server_name'), 'e', Design::erstelleEingabezeile($simple, $server, 'data[SV][name]', $server, false), 'v');
         $text .= Design::erstelleZeile($simple, Sprachen::Get('general_informations','url'), 'e', Design::erstelleEingabezeile($simple, $data['PL']['url'], 'data[PL][url]', 'http://localhost/uebungsplattform', true), 'v');
         $text .= Design::erstelleZeile($simple, Sprachen::Get('general_informations','urlExtern'), 'e', Design::erstelleEingabezeile($simple, $data['PL']['urlExtern'], 'data[PL][urlExtern]', 'http://MyURL.de/uebungsplattform', true), 'v');
-        $text .= Design::erstelleZeile($simple, Sprachen::Get('general_informations','temp'), 'e', Design::erstelleEingabezeile($simple, $data['PL']['temp'], 'data[PL][temp]', '/temp', true), 'v');
-        $text .= Design::erstelleZeile($simple, Sprachen::Get('general_informations','files'), 'e', Design::erstelleEingabezeile($simple, $data['PL']['files'], 'data[PL][files]', '/var/www/uebungsplattform/files', true), 'v');
+        $text .= Design::erstelleZeile($simple, Sprachen::Get('general_informations','temp'), 'e', Design::erstelleEingabezeile($simple, $data['PL']['temp'], 'data[PL][temp]', '/var/www/temp', true), 'v');
+        $text .= Design::erstelleZeile($simple, Sprachen::Get('general_informations','files'), 'e', Design::erstelleEingabezeile($simple, $data['PL']['files'], 'data[PL][files]', '/var/www/files', true), 'v');
 
         echo Design::erstelleBlock($simple, Sprachen::Get('general_informations','title'), $text);
     } else {
         $text = '';
         $text .= Design::erstelleVersteckteEingabezeile($simple, $data['PL']['url'], 'data[PL][url]', 'http://localhost/uebungsplattform', true);
         $text .= Design::erstelleVersteckteEingabezeile($simple, $data['PL']['urlExtern'], 'data[PL][urlExtern]', 'http://MyURL.de/uebungsplattform', true);
-        $text .= Design::erstelleVersteckteEingabezeile($simple, $data['PL']['temp'], 'data[PL][temp]', '/temp', true);
-        $text .= Design::erstelleVersteckteEingabezeile($simple, $data['PL']['files'], 'data[PL][files]', '/var/www/uebungsplattform/files', true);
+        $text .= Design::erstelleVersteckteEingabezeile($simple, $data['PL']['temp'], 'data[PL][temp]', '/var/www/temp', true);
+        $text .= Design::erstelleVersteckteEingabezeile($simple, $data['PL']['files'], 'data[PL][files]', '/var/www/files', true);
         echo $text;
     }
 #endregion Grundinformationen
