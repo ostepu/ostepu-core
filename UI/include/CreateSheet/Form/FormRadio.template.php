9e98f9086ece13e65d61c2d7946a78d2ba1fab05
Till Uhlig <till.uhlig@student.uni-halle.de>
2014-10-10 22:45:08 +0200

    deleteForm bug fixed

--- UI/include/CreateSheet/Form/FormRadio.template.php
+++ UI/include/CreateSheet/Form/FormRadio.template.php
@@ -7,7 +7,7 @@
  ?>
 <input type="hidden" class="input-choice" name="exercises[0][subexercises][0][type]" value="1">
 <label class="short label bold" for="task">Aufgabenstellung:</label>
-<textarea id="task" name="exercises[0][subexercises][0][task]"
+<textarea name="exercises[0][subexercises][0][task]"
                               class="form-field task-field"
                               rows="5"
                               style="width:100%;"
@@ -16,7 +16,7 @@
 <a href="javascript:void(0);" class="body-option-color add-choice left">Auswahl hinzufügen</a>
             
 <br><br><label class="short label bold" for="solution">Lösungsbegründung:</label>
-<textarea id="solution" name="exercises[0][subexercises][0][solution]"
+<textarea name="exercises[0][subexercises][0][solution]"
                               class="form-field"
                               rows="5"
                               style="width:100%"
