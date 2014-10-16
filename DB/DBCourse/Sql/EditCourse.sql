af9ef35b4a5454f735822ffb6b3cb2fa301cdc13
Till Uhlig <till.uhlig@student.uni-halle.de>
2014-10-16 18:12:09 +0200

    comment fixed

--- DB/DBCourse/Sql/EditCourse.sql
+++ DB/DBCourse/Sql/EditCourse.sql
@@ -3,12 +3,12 @@
  * @file EditCourse.sql
  * updates a specified course from %Course table
  * @author  Till Uhlig
- * @param int \$courseid a %Course identifier
- * @param string <?php echo $values; ?> the input data, e.g. 'a=1, b=2'
+ * @param int $courseid a %Course identifier
+ * @param string $values the input data, e.g. 'a=1, b=2'
  * @result -
  */
 ?>
  
 UPDATE Course
 SET <?php echo $values; ?>
-WHERE C_id = '<?php echo $courseid; ?>'
+WHERE C_id = '<?php echo $courseid; ?>';
