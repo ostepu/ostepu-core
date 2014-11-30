de5dc359e5ac4de3a5e526911882be45a655e27f
Till Uhlig <till.uhlig@student.uni-halle.de>
2014-11-30 19:06:26 +0100

    adds sql templates for calls to remove file types by exercise-id  and sheet-id

---
+++ DB/DBExerciseFileType/Sql/DeleteExerciseSheetExerciseFileType.sql
@@ -0,0 +1,13 @@
+<?php
+/** 
+ * @file DeleteExerciseSheetExerciseFileType.sql
+ * deletes specified exercise file types from %ExerciseFileType table
+ * @author  Till Uhlig
+ * @param int \$esid a %ExerciseSheet identifier
+ * @result -
+ */
+?>
+    
+DELETE EFT FROM ExerciseFileType EFT, Exercise E
+WHERE
+    E.ES_id = '<?php echo $esid; ?>' and E.E_id = EFT.E_id;
