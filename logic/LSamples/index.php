4c12cb51682cb4ec3daf0b1e5cf9378c5ab88aa9
Till Uhlig <till.uhlig@student.uni-halle.de>
2015-03-01 12:29:09 +0100

    adds LSamples component (no functionality)

---
+++ logic/LSamples/index.php
@@ -0,0 +1,15 @@
+<?php
+
+
+/**
+ * @file index.php executes the LSamples component on calling via rest api
+ *
+ * @author Till Uhlig
+ * @date 2015
+ */
+ 
+require_once ( dirname( __FILE__ ) . '/LSamples.php' );
+include_once ( '../../Assistants/CConfig.php' );
+
+new LSamples();
+?>
