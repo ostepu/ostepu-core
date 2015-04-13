e77df5e57d81e85ef398ec2c93b2183b6aacfc8a
Till Uhlig <till.uhlig@student.uni-halle.de>
2015-04-13 21:01:30 +0200

    reworked for groups

--- UI/UploadHistory.php
+++ UI/UploadHistory.php
@@ -10,6 +10,15 @@
 
 include_once 'include/Boilerplate.php';
 
+if (isset($_POST['sheetID']))
+    $sid = $_POST['sheetID'];
+if (isset($sid)){
+    $sheetID = $sid;
+    $_POST['sheetID'] = $sid;
+}
+if (isset($_GET['action']) && !isset($_POST['action']))
+    $_POST['action'] = $_GET['action'];
+
 // updates the selectedSubmissions for the group
 if (isset($_POST['updateSelectedSubmission'])) {
     $obj = json_decode($_POST['updateSelectedSubmission'],true);
@@ -21,7 +30,8 @@ if (isset($_POST['updateSelectedSubmission'])) {
                              $obj['leaderId'],
                              $obj['id'],
                              $obj['exerciseId'],
-                             $message);
+                             $message,
+                             1);
 
     if ($message != "201") {
         $RequestError = true;
@@ -36,23 +46,6 @@ if (isset($_POST['updateSelectedSubmission'])) {
     }
 }
     
-if (!isset($_POST['actionSortUsers']))
-if (isset($_POST['action'])) {
-    if ($_POST['action'] == "ShowUploadHistory") {
-        if (isset($_POST['userID']) && isset($_POST['sheetID'])) {
-            $uploadUserID = cleanInput($_POST['userID']);
-            $sheetID = cleanInput($_POST['sheetID']);
-
-            // loads the upload history of the selected user (uploadUserID) in the 
-            // selected course from GetSite
-            $URL = $getSiteURI . "/uploadhistory/user/{$uid}/course/{$cid}/exercisesheet/{$sheetID}/uploaduser/{$uploadUserID}";
-            $uploadHistory_data = http_get($URL, true);
-            $uploadHistory_data = json_decode($uploadHistory_data, true);
-            $uploadHistory_data['filesystemURI'] = $filesystemURI;
-        }
-    }
-}
-
 // loads data for the settings element
 $URL = $getSiteURI . "/uploadhistoryoptions/user/{$uid}/course/{$cid}";
 $uploadHistoryOptions_data = http_get($URL, true);
@@ -79,10 +72,45 @@ if (isset($_POST['sortUsers']))
     $uploadHistoryOptions_data['sortUsers'] = $_POST['sortUsers'];
 
 $user_course_data = $uploadHistoryOptions_data['user'];
-Authentication::checkRights(PRIVILEGE_LEVEL::ADMIN, $cid, $uid, $user_course_data);
+
+if (isset($user_course_data['courses'][0]['status'])){   
+    $courseStatus = $user_course_data['courses'][0]['status'];
+} else
+    $courseStatus =  -1;
+
+if ($courseStatus==0)
+    $_POST['userID'] = $uid;
+
+Authentication::checkRights(PRIVILEGE_LEVEL::STUDENT, $cid, $uid, $user_course_data);
 $menu = MakeNavigationElement($user_course_data,
-                              PRIVILEGE_LEVEL::ADMIN,
+                              PRIVILEGE_LEVEL::STUDENT,
                               true);
+                              
+if ($courseStatus<=0 /* PRIVILEGE_LEVEL::STUDENT */){
+    // extract the correct sheet
+    $sheet=null;
+    foreach ($uploadHistoryOptions_data['sheets'] as $key => $value){
+        if ($value['id'] == $sheetID){
+            $sheet = $value;
+            break;
+        }
+    }
+    
+    if ($sheet!==null && isset($sheet['endDate']) && isset($sheet['startDate'])){
+        // bool if endDate of sheet is greater than the actual date
+        $isExpired = date('U') > date('U', $sheet['endDate']); 
+
+        // bool if startDate of sheet is greater than the actual date
+        $hasStarted = date('U') > date('U', $sheet['startDate']);
+        if ($isExpired){
+            set_error("Der Übungszeitraum ist am ".date('d.m.Y  -  H:i', $sheet['endDate'])." abgelaufen!");
+        } elseif (!$hasStarted){
+            set_error("Der Übungszeitraum beginnt am ".date('d.m.Y  -  H:i', $sheet['startDate'])."!");
+        }
+        
+    } else
+        set_error("Kein Übungszeitraum gefunden!");
+}
 
 // construct a new header
 $h = Template::WithTemplateFile('include/Header/Header.template.html');
@@ -91,20 +119,47 @@ $h->bind(array("name" => $user_course_data['courses'][0]['course']['name'],
                "notificationElements" => $notifications,
                "navigationElement" => $menu));
 
+if (!isset($_POST['actionSortUsers']))
+if (isset($_POST['action'])) {
+    if ($_POST['action'] == "ShowUploadHistory") {
+        if (isset($_POST['userID']) && (isset($_POST['sheetID']) || isset($sheetID))) {
+            $uploadUserID = cleanInput($_POST['userID']);
+            
+            if (isset($_POST['sheetID']))
+                $sheetID = cleanInput($_POST['sheetID']);
+
+            // loads the upload history of the selected user (uploadUserID) in the 
+            // selected course from GetSite
+            $URL = $getSiteURI . "/uploadhistory/user/{$uid}/course/{$cid}/exercisesheet/{$sheetID}/uploaduser/{$uploadUserID}";
+            $uploadHistory_data = http_get($URL, true);
+            $uploadHistory_data = json_decode($uploadHistory_data, true);
+            $uploadHistory_data['filesystemURI'] = $filesystemURI;
+        }
+    }
+}
 
 // construct a content element for the ability to look at the upload history of a student
-$uploadHistorySettings = Template::WithTemplateFile('include/UploadHistory/UploadHistorySettings.template.html');
-$uploadHistorySettings->bind($uploadHistoryOptions_data);
+if ($courseStatus>=1 /* PRIVILEGE_LEVEL::TUTOR */){
+    $uploadHistorySettings = Template::WithTemplateFile('include/UploadHistory/UploadHistorySettings.template.html');
+    $uploadHistorySettings->bind($uploadHistoryOptions_data);
+}
 
 $uploadHistory = Template::WithTemplateFile('include/UploadHistory/UploadHistory.template.html');
 if (isset($uploadHistory_data))$uploadHistory->bind($uploadHistory_data);
 if (isset($uploadHistoryNotifications))
     $uploadHistory->bind(array("UploadHistoryNotificationElements" => $uploadHistoryNotifications));
 
+if ($courseStatus >= 1 /* PRIVILEGE_LEVEL::TUTOR */){
+    $uploadHistoryGroup = Template::WithTemplateFile('include/UploadHistory/UploadHistoryGroup.template.html');
+    if (isset($uploadHistory_data))$uploadHistoryGroup->bind($uploadHistory_data);
+    if (isset($uploadHistoryGroupNotifications))
+        $uploadHistoryGroup->bind(array("UploadHistoryNotificationElements" => $uploadHistoryGroupNotifications));
+}
+
 // wrap all the elements in some HTML and show them on the page
-$w = new HTMLWrapper($h, $uploadHistorySettings, $uploadHistory);
-$w->defineForm(basename(__FILE__)."?cid=".$cid, false, $uploadHistorySettings);
-$w->defineForm(basename(__FILE__)."?cid=".$cid, false, $uploadHistory);
+$w = new HTMLWrapper($h, isset($uploadHistorySettings) ? $uploadHistorySettings : null, isset($uploadHistoryGroup) ? $uploadHistoryGroup : null, isset($uploadHistory) ? $uploadHistory : null);
+if (isset($uploadHistorySettings)) $w->defineForm(basename(__FILE__)."?cid=".$cid, false, $uploadHistorySettings);
+if (isset($uploadHistory))$w->defineForm(basename(__FILE__)."?cid=".$cid, false, $uploadHistory);
 $w->set_config_file('include/configs/config_default.json');
 $w->show();
 
