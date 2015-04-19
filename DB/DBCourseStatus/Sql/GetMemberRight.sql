aa084de5edabcc9c4b4b28cc5b3e64e6d9b73d90
Till Uhlig <till.uhlig@student.uni-halle.de>
2015-04-19 10:47:59 +0200

    - adds settings to setting-tabel - sql statements improved to support DBSetting - corrects failure in the settings extraction

--- DB/DBCourseStatus/Sql/GetMemberRight.sql
+++ DB/DBCourseStatus/Sql/GetMemberRight.sql
@@ -27,7 +27,8 @@
     C.C_id,
     C.C_name,
     C.C_semester,
-    C.C_defaultGroupSize
+    C.C_defaultGroupSize,
+    S.*
 from
 
     CourseStatus CS 
@@ -36,5 +37,7 @@ from
  join
     User U
        ON (U.U_id = CS.U_id)
+        left join 
+    Setting_<?php echo $courseid; ?> S ON (1)
 WHERE
     CS.U_id = '<?php echo $userid; ?>' and CS.C_id = '<?php echo $courseid; ?>'
