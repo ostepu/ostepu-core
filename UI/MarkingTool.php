af8b1be38227b9b6aa5a9e699facd0b4417bf8d0
Till Uhlig <till.uhlig@student.uni-halle.de>
2015-05-12 12:22:16 +0200

    shows deviation from closing date for submissions

--- UI/MarkingTool.php
+++ UI/MarkingTool.php
@@ -496,6 +496,17 @@ $searchSettings->bind($markingTool_data);
 $w = new HTMLWrapper($h, $searchSettings);
 
 if (!empty($markingTool_data['groups'])) {
+// find selected sheet
+$selectedSheet=null;
+foreach ($markingTool_data['exerciseSheets'] as $sheet){
+    if (!isset($sheet['id'])) continue;
+    if ($sheet['id'] == $sid){
+        $selectedSheet = $sheet;
+        break;
+    }
+}
+
+    
 $allOutputs = 0;
 $groups = $markingTool_data['groups'];
 //unset($markingTool_data['groups']);
@@ -513,6 +524,7 @@ $groups = $markingTool_data['groups'];
     
         $markingElement = Template::WithTemplateFile('include/MarkingTool/MarkingTool.template.html');
         $markingElement->bind($markingTool_data);
+        $markingElement->bind(array('selectedSheet'=>$selectedSheet));
         $markingElement->bind(array('group'=>$group));
         if (isset($GroupNotificationElements[$group['leader']['id']])){
             $markingElement->bind(array('GroupNotificationElements'=>$GroupNotificationElements[$group['leader']['id']]));
