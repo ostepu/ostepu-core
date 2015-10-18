a843c6939001bbd1aaec61d4b8c9eb69d241a0b8
Till Uhlig <till.uhlig@student.uni-halle.de>
2015-10-19 00:39:16 +0200

    - nicht exitierende oder leere Dateien werden als "kein Inhalt!!!" ausgegeben - formatiert - in Commands.json einen regulären Ausdruck für path+ eingefügt

--- DB/CHelp/CHelp.php
+++ DB/CHelp/CHelp.php
@@ -73,21 +73,37 @@ class CHelp
         }
         
         $order = implode('/',$params['path']);
-        $order = '/help/'.$order;
+        $order = '/help/'.$order;      
         
-        
-        $positive = function($input, $cachePath, $realExtension) {
+        $positive = function($input, $cachePath, $realExtension, $negativeMethod) {
+            if (empty($input)){
+                // wenn die zurückgegebene Datei leer ist, wird nicht gecached und die negative Methode aufgerufen
+                return call_user_func_array($negativeMethod, array());
+            }
+            
             if ($realExtension == '.md'){
                 $parser = new \Michelf\MarkdownExtra;
                 $input = $this->umlaute($input);
                 $my_html = $parser->transform($input);
-                $input = '<link rel="stylesheet" href="'.$this->config['MAIN']['externalUrl'].'/UI/css/github-markdown.css" type="text/css"><span class="markdown-body">'.$my_html.'</span>';
+                $input = '<html><head></head><body><link rel="stylesheet" href="'.$this->config['MAIN']['externalUrl'].'/UI/css/github-markdown.css" type="text/css"><span class="markdown-body">'.$my_html.'</span></body></html>';
             }
             
+            // die Hilfedatei wird lokal gespeichert
             @file_put_contents($cachePath,$input);
             return Model::isOk($input);
         };
-        return $this->_component->callByURI('request', $order, array('language'=>$params['language']), '', 200, $positive, array('cachePath'=>$cachePath, 'realExtension'=>$realExtension), 'Model::isEmpty', array());
+        
+        $negative = function() {
+            $input = '#### !!!Kein Inhalt!!!';
+            $parser = new \Michelf\MarkdownExtra;
+            $input = $this->umlaute($input);
+            $my_html = $parser->transform($input);
+            $input = '<html><head></head><body><link rel="stylesheet" href="'.$this->config['MAIN']['externalUrl'].'/UI/css/github-markdown.css" type="text/css"><span class="markdown-body">'.$my_html.'</span></body></html>';
+
+            return Model::isOk($input);
+        };
+        
+        return $this->_component->callByURI('request', $order, array('language'=>$params['language']), '', 200, $positive, array('cachePath'=>$cachePath, 'realExtension'=>$realExtension, 'negativeMethod'=>$negative), $negative, array());
     }
     
     /**
