c32fb154ce57d723203c6ee3e98f4499bb3d1e12
Till Uhlig <till.uhlig@student.uni-halle.de>
2015-11-01 15:18:23 +0100

    erste Implementierung von #172

---
+++ install/segments/GitAktualisierung.php
@@ -0,0 +1,129 @@
+<?php
+#region GitAktualisierung
+class GitAktualisierung
+{
+    public static $name = 'GitAktualisierung';
+    public static $installed = false;
+    public static $enabledShow = true;
+    public static $rank = 25;
+    public static $page = 0;
+    private static $initialized=false;
+    
+    public static $onEvents = array('collect'=>array('procedure'=>'collect','name'=>'collectGitUpdates','event'=>array('actionCollectGitUpdates')),
+                                    'install'=>array('procedure'=>'install','name'=>'installGitUpdates','event'=>array('actionInstallGitUpdates')));
+    
+    public static function show($console, $result, $data)
+    {
+        $text='';       
+        if (!$console)
+            $text .= Design::erstelleBeschreibung($console,Language::Get('gitUpdate','description'));
+
+        $collected = array();
+        if (isset($result[self::$onEvents['collect']['name']]) && $result[self::$onEvents['collect']['name']]!=null){
+           $collected =  $result[self::$onEvents['collect']['name']];
+        } else 
+           $collected = array('content'=>null,'fail'=>false,'errno'=>null,'error'=>null);
+        
+        if (Einstellungen::$accessAllowed){
+            //if ($collected['content'] === null){
+                if (!$console)
+                    $text .= Design::erstelleZeile($console, Language::Get('gitUpdate','collectGitUpdates'), 'e', Design::erstelleSubmitButton(self::$onEvents['collect']['event'][0], Language::Get('gitUpdate','collectGitUpdates')), 'h');
+            //}
+            
+            
+            if (isset($collected['content']['modified']) && $collected['content']['modified'] !== null){
+                $t = '';
+                if (isset($collected['content']['modified'][0])){
+                    $t = $collected['content']['modified'][0];
+                } else {
+                    $t = '----';                    
+                }
+                
+                if (!$console){
+                    $text .= "<tr><td class='e' colspan='3'>{$t}</td></tr>";
+                } else  {
+                    
+                }
+            }
+            
+            if (isset($collected['content']['commits']) && $collected['content']['commits'] !== null){
+                for($i=0;$i<10 && $i<count($collected['content']['commits']);$i++){
+                    if (!$console){
+                        $text .= "<tr><td class='v' colspan='2'>{$collected['content']['commits'][$i]['desc']}</td><td class='e'>{$collected['content']['commits'][$i]['period']}</td></tr>";
+                    } else  {
+                        
+                    }
+                }
+                if (count($collected['content']['commits'])>10){
+                    if (!$console){
+                        $text .= Design::erstelleZeile($console, '...', 'v');
+                    } else  {
+                        
+                    }  
+                }
+            }
+        
+        }  
+        
+        echo Design::erstelleBlock($console, Language::Get('gitUpdate','title'), $text);
+        return null;
+    }
+    
+    public static function init($console, &$data, &$fail, &$errno, &$error)
+    {
+        self::$initialized = true;
+    }
+    
+    public static function collect($data, &$fail, &$errno, &$error)
+    {
+        $result = array('commits'=>null, 'modified'=>null);
+        $pathOld = getcwd();
+        $output=null;
+        chdir(dirname(__FILE__).'/../../');
+        exec('(git fetch) 2>&1', $output, $return);
+        chdir($pathOld);
+        
+        if ($return == 0){
+            $pathOld = getcwd();
+        $output=null;
+            chdir(dirname(__FILE__).'/../../');
+            exec('(git diff --shortstat HEAD...FETCH_HEAD) 2>&1', $output, $return);
+            chdir($pathOld);
+            
+            if ($return == 0){
+                $result['modified'] = $output;
+                
+                $pathOld = getcwd();
+                $output=null;
+                chdir(dirname(__FILE__).'/../../');
+                exec('(git log --pretty=format:\'%s,%cr\' --abbrev-commit --date=relative HEAD...FETCH_HEAD) 2>&1', $output, $return);
+                chdir($pathOld);
+                
+                if ($return == 0){
+                    $result['commits'] = array();
+                    foreach($output as $out){
+                        $period = substr(strrchr ( $out , ',' ),1);
+                        $period = trim($period, "'");
+                        $description = substr( $out, 0,strlen($out) - strlen($period) - 1 );
+                        $description = trim($description, "'");
+                        $element = array('desc'=>$description, 'period'=>$period);
+                        $result['commits'][] = $element;
+                    }
+                } else {
+                    $fail = true;
+                    $error = Language::Get('gitUpdate','errorGitDiff');                
+                }
+            } else {
+                $fail = true;
+                $error = Language::Get('gitUpdate','errorGitDiff');                
+            }
+            
+        } else {
+            $fail = true;
+            $error = Language::Get('gitUpdate','errorGitFetch');
+        }
+        
+        return $result;
+    }
+}
+#endregion GitAktualisierung
