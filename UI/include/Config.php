656432807fb30ebe0ed369f49c46be41722dd789
Till Uhlig <till.uhlig@student.uni-halle.de>
2015-11-26 16:45:23 +0100

    Config.php entfernt

--- UI/include/Config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$serverURI = 'http://localhost/uebungsplattform';
-$databaseURI = $serverURI . "/DB/DBControl";
-$logicURI = $serverURI . "/logic/LController";
-$logicFileURI = $serverURI . "/logic/LFile";
-$filesystemURI = $serverURI . "/FS/FSControl";
-$getSiteURI = $serverURI . "/logic/LGetSite";
-$globalSiteKey = 'b67dc54e7d03a9afcd16915a55edbad2d20a954562c482de3863456f01a0dee4';
