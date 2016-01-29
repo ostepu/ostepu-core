80bbe04f44e93a3f8f157d536a50d29c81db1c65
Till Uhlig <till.uhlig@student.uni-halle.de>
2016-01-30 00:33:41 +0100

    zeigt existierende Backups an

--- install/segments/BackupSegment.php
+++ install/segments/BackupSegment.php
@@ -16,13 +16,21 @@ class BackupSegment
                                         'event'=>array('actionCreateImage'),
                                         'procedure'=>'installCreateImage',
                                         'enabledInstall'=>true
+                                        ),
+                                    'listImage'=>array(
+                                        'name'=>'listImage',
+                                        'event'=>array('actionListImage'),
+                                        'procedure'=>'installListImage',
+                                        'enabledInstall'=>true
                                         )
                                     );
 
     public static function getDefaults()
     {
         $res = array(
-                     'path' => array('data[BACK][path]', '/var/www/backup')
+                     'path' => array('data[BACK][path]', '/var/www/backup'),
+                     'database' => array('data[BACK][database]', 'enabled'),
+                     'files' => array('data[BACK][files]', 'enabled')
                      );
         return $res;
     }
@@ -32,13 +40,15 @@ class BackupSegment
         Installation::log(array('text'=>Installation::Get('main','functionBegin')));
         Language::loadLanguageFile('de', self::$langTemplate, 'json', dirname(__FILE__).'/');
         Installation::log(array('text'=>Installation::Get('main','languageInstantiated')));
-       
+     
         $def = self::getDefaults();
-        
+      
         $text = '';
         $text .= Design::erstelleVersteckteEingabezeile($console, $data['BACK']['path'], 'data[BACK][path]', $def['path'][1], true);
+        $text .= Design::erstelleVersteckteEingabezeile($console, $data['BACK']['database'], 'data[BACK][database]', $def['database'][1], true);
+        $text .= Design::erstelleVersteckteEingabezeile($console, $data['BACK']['files'], 'data[BACK][files]', $def['files'][1], true);
         echo $text;
-        
+      
         self::$initialized = true;
         Installation::log(array('text'=>Installation::Get('main','functionEnd')));
     }
@@ -50,21 +60,77 @@ class BackupSegment
         Installation::log(array('text'=>Installation::Get('main','functionBegin')));
         $text='';
         $text .= Design::erstelleBeschreibung($console,Installation::Get('main','description',self::$langTemplate));
-        
+      
         $text .= Design::erstelleZeile($console, Installation::Get('createImage','imagePath',self::$langTemplate), 'e', Design::erstelleEingabezeile($console, $data['BACK']['path'], 'data[BACK][path]', $data['BACK']['path'], true), 'v');
+        $text .= Design::erstelleZeile($console, Installation::Get('createImage','packDatabase',self::$langTemplate), 'e', Design::erstelleAuswahl($console, $data['BACK']['database'], 'data[BACK][database]', 'enabled', null, true), 'v_c');
+        $text .= Design::erstelleZeile($console, Installation::Get('createImage','packFiles',self::$langTemplate), 'e', Design::erstelleAuswahl($console, $data['BACK']['files'], 'data[BACK][files]', 'enabled', null, true), 'v_c');
         if (self::$onEvents['createImage']['enabledInstall']){
             $text .= Design::erstelleZeile($console, Installation::Get('createImage','createImageDesc',self::$langTemplate), 'e',  Design::erstelleSubmitButton(self::$onEvents['createImage']['event'][0],Installation::Get('createImage','createImage',self::$langTemplate)), 'h');
         }
-        
+      
         $createBackup=false;
         if (isset($result[self::$onEvents['createImage']['name']])){
             $content = $result[self::$onEvents['createImage']['name']]['content'];
+            if (!isset($content['databaseOutput'])) $content['databaseOutput'] = '';
+            if (!isset($content['databaseOutputStatus'])) $content['databaseOutputStatus'] = 1;
+            if (!isset($content['databaseOutputSize'])) $content['databaseOutputSize'] = 0;
+            if (!isset($content['filesOutputSize'])) $content['filesOutputSize'] = 0;
+            if (!isset($content['filesOutputAmount'])) $content['filesOutputAmount'] = 0;
+            if (!isset($content['file'])) $content['file'] = '???';
+          
             $createBackup=true;
             if (!empty($content['output'])){
-                $text .= Design::erstelleZeile($console, Installation::Get('createImage','message',self::$langTemplate) , 'e', $content['output'], 'v error_light break');
+                $text .= Design::erstelleZeile($console, Installation::Get('createImage','databaseMessage',self::$langTemplate) , 'e', $content['databaseOutput'], 'v error_light break');
             }
-            $text .= Design::erstelleZeile($console, Installation::Get('createImage','status',self::$langTemplate) , 'e', ($content['outputStatus'] == 0 ? Installation::Get('main','ok') : Installation::Get('main','fail')), 'v_c');
-            $text .= Design::erstelleZeile($console, Installation::Get('createImage','filePath',self::$langTemplate) , 'e', $content['file'], 'v');
+          
+            if (isset($data['BACK']['database']) && $data['BACK']['database'] == 'enabled'){
+                $text .= Design::erstelleZeile($console, Installation::Get('createImage','databaseStatus',self::$langTemplate) , 'e', ($content['databaseOutputStatus'] == 0 ? Installation::Get('main','ok') : Installation::Get('main','fail')), 'v_c');
+                $text .= Design::erstelleZeile($console, Installation::Get('createImage','databaseSize',self::$langTemplate) , 'e', Design::formatBytes($content['databaseOutputSize']), 'v');
+            }
+          
+            if (isset($data['BACK']['files']) && $data['BACK']['files'] == 'enabled'){
+                $text .= Design::erstelleZeile($console, Installation::Get('createImage','filesAmount',self::$langTemplate) , 'e', $content['filesOutputAmount'], 'v');
+                $text .= Design::erstelleZeile($console, Installation::Get('createImage','filesSize',self::$langTemplate) , 'e', Design::formatBytes($content['filesOutputSize']), 'v');
+            }
+          
+            $text .= Design::erstelleZeile($console, Installation::Get('createImage','filePath',self::$langTemplate) , 'e', $content['outputFile'], 'v');
+            $text .= Design::erstelleZeile($console, Installation::Get('createImage','fileSize',self::$langTemplate) , 'e', Design::formatBytes($content['outputSize']), 'v');
+        }      
+      
+        if (self::$onEvents['listImage']['enabledInstall']){
+            $text .= Design::erstelleZeile($console, Installation::Get('listImage','listImageDesc',self::$langTemplate), 'e',  Design::erstelleSubmitButton(self::$onEvents['listImage']['event'][0],Installation::Get('listImage','listImage',self::$langTemplate)), 'h');
+        }
+      
+      
+        if (isset($result[self::$onEvents['listImage']['name']])){
+            $content = $result[self::$onEvents['listImage']['name']]['content'];
+            foreach($content['backups'] as $key => $backup){
+                if ($key == 0){
+                    $text .= Design::erstelleZeile($console, '','','','' );
+                }
+              
+                $text .= Design::erstelleZeile($console, Installation::Get('listImage','file',self::$langTemplate) , 'e', $backup['file'], 'v');
+                if (isset($backup['conf']['date'])){
+                    $text .= Design::erstelleZeile($console, Installation::Get('listImage','date',self::$langTemplate) , 'e', date('d.m.Y - H:i:s',$backup['conf']['date']), 'v');
+                }
+                if (isset($backup['conf']['database']['size'])){
+                    $text .= Design::erstelleZeile($console, Installation::Get('listImage','databaseSize',self::$langTemplate) , 'e', Design::formatBytes($backup['conf']['database']['size']), 'v');
+                }
+                if (isset($backup['conf']['files']['size'])){
+                    $text .= Design::erstelleZeile($console, Installation::Get('listImage','filesSize',self::$langTemplate) , 'e', Design::formatBytes($backup['conf']['files']['size']), 'v');
+                }
+                if (isset($backup['size'])){
+                    $text .= Design::erstelleZeile($console, Installation::Get('listImage','imageSize',self::$langTemplate) , 'e', Design::formatBytes($backup['size']), 'v');
+                }
+              
+                if ($key != count($content['backups'])-1){
+                    $text .= Design::erstelleZeile($console, '','','','' );
+                }
+            }
+          
+            if (empty($content['backups'])){
+                $text .= Design::erstelleZeile($console, '','e',Installation::Get('listImage','noImages',self::$langTemplate),'v_c' );
+            }
         }
 
         echo Design::erstelleBlock($console, Installation::Get('main','title',self::$langTemplate), $text);
@@ -73,40 +139,101 @@ class BackupSegment
         return null;
     }
 
+    public static function installListImage($data, &$fail, &$errno, &$error)
+    {
+        Installation::log(array('text'=>Installation::Get('main','functionBegin')));
+        $res = array();
+        $backups = Installation::read_all_files($data['BACK']['path']);
+        $res['backups'] = array();
+        foreach($backups['files'] as $backup){
+            $data = array();
+            $data['file'] = $backup;
+            $zip = new ZipArchive;
+            if ( $zip->open($backup) === TRUE ){
+                $conf = $zip->getFromName('backup.conf');
+                $conf = json_decode($conf,true);
+                $data['conf'] = $conf;
+                $data['size'] = filesize($backup);
+                $zip->close();
+            } else {
+                return $res;
+            }
+                  
+            $res['backups'][] = $data;
+        }
+      
+        Installation::log(array('text'=>Installation::Get('main','functionEnd')));
+        return $res;
+    }
+  
     public static function installCreateImage($data, &$fail, &$errno, &$error)
     {
         Installation::log(array('text'=>Installation::Get('main','functionBegin')));
         $res = array();
-        
+      
         $mainPath = realpath(dirname(__FILE__) . DIRECTORY_SEPARATOR . '../..');
         $mainPath = str_replace(array("\\","/"), array(DIRECTORY_SEPARATOR,DIRECTORY_SEPARATOR), $mainPath);
         $location = $data['BACK']['path'];
         Einstellungen::generatepath($location);
         $location = realpath($location);
-        
+      
         $fileName = $data['DB']['db_name'].'_'.date('Ymd_His');
-        $filePath = $location. DIRECTORY_SEPARATOR .$fileName.'.sql';
-        $displayName = $filePath;
-        
-        $output = null;
-        $return = null;
-        $pathOld = getcwd();
-        chdir($location);                            
-        exec('(mysqldump --user '.$data['DB']['db_user_operator'].' --password="'.$data['DB']['db_passwd_operator'].'" --opt --result-file '.$filePath.' --skip-triggers --no-create-db '.$data['DB']['db_name'].') 2>&1', $output, $return);  
-        chdir($pathOld);    
-        
-        $zip = new ZipArchive( );
+        $sqlFilePath = $location. DIRECTORY_SEPARATOR .'database.sql';
+      
+        $definition = array();
+        $definition['date'] = time();
+      
+        $zip = new ZipArchive;
         if ( $zip->open($location. DIRECTORY_SEPARATOR .$fileName.'.zip',ZIPARCHIVE::CREATE) === TRUE ){
-            $zip->addFile($filePath,$fileName.'.sql');
-            $zip->close( );
-            @unlink( $filePath );
-            $displayName = $location. DIRECTORY_SEPARATOR .$fileName.'.zip';
+            // Ok
+        } else {
+            return $res;
         }
-            
-        $res['file'] = $displayName;
-        $res['output'] = $output;
-        $res['outputStatus'] = $return;
-        
+        $displayName = $location. DIRECTORY_SEPARATOR .$fileName.'.zip';
+      
+        // erzeuge ein Abbild der Datenbank
+        if (isset($data['BACK']['database']) && $data['BACK']['database'] == 'enabled'){
+            $output = null;
+            $return = null;
+            $pathOld = getcwd();
+            chdir($location);                          
+            exec('(mysqldump --user '.$data['DB']['db_user_operator'].' --password="'.$data['DB']['db_passwd_operator'].'" --opt --result-file '.$sqlFilePath.' --skip-triggers --no-create-db '.$data['DB']['db_name'].') 2>&1', $output, $return);
+            chdir($pathOld);
+            $res['databaseOutput'] = $output;
+            $res['databaseOutputStatus'] = $return;
+          
+            $zip->addFile($sqlFilePath,basename($sqlFilePath));
+            $definition['database'] = array();
+            $definition['database']['file'] = basename($sqlFilePath);
+            $definition['database']['size'] = filesize($sqlFilePath);
+            $res['databaseOutputSize'] = $definition['database']['size'];
+        }
+      
+        // erzeuge ein Abbild der Dateien
+        if (isset($data['BACK']['files']) && $data['BACK']['files'] == 'enabled'){
+            $files = Installation::read_all_files($data['PL']['files']);
+            $filesSize = 0;
+            foreach($files['files'] as $file){
+                $fileBase = substr($file,strlen($data['PL']['files'])+1);
+                $filesSize+=filesize($file);
+                $zip->addFile($file,'files/'.$fileBase);
+            }
+            $definition['files'] = array();
+            $definition['files']['folder'] = 'files';
+            $definition['files']['size'] = $filesSize;
+            $definition['files']['amount'] = count($files['files']);
+            $res['filesOutputSize'] = $filesSize;
+            $res['filesOutputAmount'] = count($files['files']);
+        }
+      
+        $zip->addFromString('backup.conf',json_encode($definition));
+      
+        $zip->close();
+        @unlink( $sqlFilePath );
+      
+        $res['outputFile'] = $displayName;
+        $res['outputSize'] = filesize($displayName);
+      
         Installation::log(array('text'=>Installation::Get('main','functionEnd')));
         return $res;
     }
