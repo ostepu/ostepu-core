2f103d92bbd97004542319798b72c040b15e5541
Ralf Busch <ralfbusch92@gmail.com>
2016-02-09 22:49:23 +0100

    Timeout in Sandbox hinzugefügt

--- Assistants/Sandbox.php
+++ Assistants/Sandbox.php
@@ -19,6 +19,11 @@ class Sandbox
     private $workingDir = "";
 
     /**
+     * @var int $timeout Timeout in Seconds, have to be under apache timeout to ensure script terminates
+     */
+    private $timeout = 240;
+
+    /**
      * the $workingDir setter
      *
      * @param string $path the new value for working directory
@@ -176,27 +181,69 @@ class Sandbox
 
             if (is_resource($process)) {
 
-              // anatomy of $pipes: 0 => stdin, 1 => stdout, 2 => error log
-              fwrite($pipes[0], 'umask 000;'.$realcmd.' '.$params.';echo "STATUS=("$?")";');
+                // anatomy of $pipes: 0 => stdin, 1 => stdout, 2 => error log
+                // Set the stdout stream to none-blocking.
+                stream_set_blocking($pipes[1], false);
 
-              fclose($pipes[0]) ;
+                // Turn the timeout into microseconds.
+                $mytimeout = $this->timeout * 1000000;
 
-              // print pipe output
-              $output = stream_get_contents($pipes[1]);
+                // Output buffer.
+                $output = '';
 
-              $error = stream_get_contents($pipes[2]);
 
-              // close pipe
-              fclose($pipes[1]) ;
-              fclose($pipes[2]) ;
-             
-              // all pipes must be closed before calling proc_close. 
-              // proc_close() to avoid deadlock
-              proc_close($process) ;
+                fwrite($pipes[0], 'umask 000;'.$realcmd.' '.$params.';echo "STATUS=("$?")";');
 
-              // pregmatches erst nachdem stream geschlossen
-              preg_match("/STATUS=\([0-9]+\)/", $output, $treffer);
-              $output = preg_replace("/STATUS=\([0-9]+\)/", "", $output);
+                fclose($pipes[0]);
+
+                // While we have time to wait.
+                while ($mytimeout > 0) {
+                    $start = microtime(true);
+
+                    // Wait until we have output or the timer expired.
+                    $read  = array($pipes[1]);
+                    $other = array();
+                    $noTimeout = stream_select($read, $other, $other, 0, $mytimeout);
+
+                    // Get the status of the process.
+                    // Do this before we read from the stream,
+                    // this way we can't lose the last bit of output if the process dies between these     functions.
+                    $status = proc_get_status($process);
+
+                    // Read the contents from the buffer.
+                    // This function will always return immediately as the stream is none-blocking.
+                    $output .= stream_get_contents($pipes[1]);
+
+                    if (!$status['running']) {
+                      // Break from this loop if the process exited before the timeout.
+                      break;
+                    }
+
+                    // Subtract the number of microseconds that we waited.
+                    $mytimeout -= (microtime(true) - $start) * 1000000;
+                }
+
+                // print pipe output
+                /*$output = stream_get_contents($pipes[1]);*/
+
+                $error = stream_get_contents($pipes[2]);
+
+                // Kill the process in case the timeout expired and it's still running.
+                // If the process already exited this won't do anything.
+                proc_terminate($process, 9);
+
+
+                // close pipe
+                fclose($pipes[1]);
+                fclose($pipes[2]);
+            
+                // all pipes must be closed before calling proc_close. 
+                // proc_close() to avoid deadlock
+                proc_close($process) ;
+
+                // pregmatches erst nachdem stream geschlossen
+                preg_match("/STATUS=\([0-9]+\)/", $output, $treffer);
+                $output = preg_replace("/STATUS=\([0-9]+\)/", "", $output);
             }
             chdir($pathOld);
 
@@ -221,6 +268,9 @@ class Sandbox
             else
             {
                 $status = 1;
+                if ($noTimeout == false) {
+                    $output = "Exit with Timeout.";
+                }
             }
             
 
