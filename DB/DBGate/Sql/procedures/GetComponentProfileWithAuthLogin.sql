e51ed379eaecb8cb7b171862dd2c3c504af2c27f
Till Uhlig <till.uhlig@student.uni-halle.de>
2016-11-24 18:20:45 +0100

    DBGate: GetComponentProfileWithAuth und GetComponentProfileWithAuthLogin arbeitet nun mit dem Namen des Profils, nicht mit der ID

--- DB/DBGate/Sql/procedures/GetComponentProfileWithAuthLogin.sql
+++ DB/DBGate/Sql/procedures/GetComponentProfileWithAuthLogin.sql
@@ -1,5 +1,5 @@
 DROP PROCEDURE IF EXISTS `DBGateGetComponentGateProfileWithAuthLogin`;
-CREATE PROCEDURE `DBGateGetComponentGateProfileWithAuthLogin` (IN profile varchar(30), IN authProfile varchar(30), IN ruleProfile varchar(30), IN gpid INT, IN authType varchar(120), IN component varchar(120), IN login varchar(120))
+CREATE PROCEDURE `DBGateGetComponentGateProfileWithAuthLogin` (IN profile varchar(30), IN authProfile varchar(30), IN ruleProfile varchar(30), IN profName varchar(30), IN authType varchar(120), IN component varchar(120), IN login varchar(120))
 READS SQL DATA
 begin
 SET @s = concat("
@@ -21,7 +21,7 @@ FROM
         left join
     `GateRule",ruleProfile,"` GR ON (GP.GP_id = GR.GP_id)
 WHERE
-    GP.GP_id = '",gpid,"' and (GA.GA_type = '",authType,"' or GA.GA_type='noAuth') and GR.GR_component = '",component,"' and GA.GA_login = '",login,"';");
+    GP.GP_name = '",profName,"' and (GA.GA_type = '",authType,"' or GA.GA_type='noAuth') and GR.GR_component = '",component,"' and GA.GA_login = '",login,"';");
 PREPARE stmt1 FROM @s;
 EXECUTE stmt1;
 DEALLOCATE PREPARE stmt1;
