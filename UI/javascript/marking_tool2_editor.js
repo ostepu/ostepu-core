1f46f7ff5b6f937825766ae4cd0e65951420e026
Max Brauer <ma.brauer@live.de>
2017-02-21 00:34:22 +0100

    Zur Marking Tool 2 JS Bibliothek wurde ein Eventsystem und eine komplette Objektüberwachung hinzugefügt. Außerdem wurden kleinere Fehler entfernt.

--- UI/javascript/marking_tool2_editor.js
+++ UI/javascript/marking_tool2_editor.js
@@ -4,6 +4,44 @@
 var MarkingTool = MarkingTool || {};
 MarkingTool.Editor = MarkingTool.Editor || {};
 
+//=== Hilfsbibliotheken
+
+//Ein Eventhandler, welcher ein Event verwalten kann.
+MarkingTool.Event = function() {
+	var list = [];
+	//Fügt eine neue Eventmethode zu diesem Event hinzu.
+	//method: function - die Methode, die beim Auslösen des Events aufgerufen wird.
+	this.add = function(method) {
+		for (var i = 0; i<list.length; ++i)
+			if (list[i] == method) return;
+		list.push(method);
+	};
+	//Entfernt eine Eventmethode wieder
+	//method: function - die Methode, die nicht mehr aufgerufen werden soll.
+	this.remove = function(method) {
+		for (var i = 0; i<list.length; ++i)
+			if (list[i] == method) {
+				list.splice(i, 1);
+				return;
+			}
+	};
+	//Ruft alle Methoden mit aktuellen Kontext und verschiedenen Argumenten auf.
+	//args: Werte... - Verschiedene Argumente
+	this.invoke = function(args) {
+		for (var i = 0; i<list.length; ++i)
+			list[i].apply(this, arguments);
+	};
+	//Ruft alle Methoden in einem bestimmten Kontext und verschiedenen Argumenten auf.
+	//thisArg: Wert     - der Zielkontext für die Methoden
+	//args:    Werte... - Verschiedene Argumente
+	this.call = function(thisArg, args) {
+		arguments.splice(0, 1);
+		for (var i = 0; i<list.length; ++i)
+			list[i].apply(thisArg, arguments);
+	};
+};
+
+
 //=== HTML Bibliothek um einen HTML Körper zu erstellen ===
 MarkingTool.Editor.HTML = (function(){
 	//private static
@@ -18,7 +56,7 @@ MarkingTool.Editor.HTML = (function(){
 			data.content = undefined;
 			data.element = undefined;
 			var obj = $("<"+element+"/>");
-			foreach (var key in data)
+			for (var key in data)
 				if (data.hasOwnProperty(key)) {
 					obj.attr(key, data[key]);
 				}
@@ -41,35 +79,23 @@ MarkingTool.Editor.HTML = (function(){
 })();
 
 //=== Update Bibliothek um immer den aktuellen Zustand von Objekten verfolgen zu können. ===
+
 //Verfolgt den Zustand einer Property eines Objektes
 //owner: Objekt - Das Objekt das eine Property enthält, die Überwacht werden soll.
 //key:   String - Der Name der Property
 //path:  Objekt - Eindeutige Informationen, die das Objekt identifizieren
-Marking.Editor.UpdateProperty = function(owner, key, path) {
+MarkingTool.Editor.UpdateProperty = function(owner, key, path) {
 	var defaultValue = undefined;
-	var raiseList = [];
+	var updadedEvent = new MarkingTool.Event();
 	var thisref = this; //Der Verweis auf dieses Objekt für spätere Eventaufrufe
 	//String - Der Name der Property
 	this.key = key;
 	//Objekt - Der Pfad, der das Besitzerobjekt identifiziert.
 	this.path = path;
-	//Fügt eine neue Methode hinzu die aufgerufen wird, sobald der Wert der Variable sich ändert.
-	//method: function(sender) - Die Methode
-	//        sender: Objekt   - Das Objekt welches diesen Event ausgelöst hat (meist dieses)
-	this.addUpdateHandler = function(method) {
-		for (var i = 0; i<raiseList.length; ++i)
-			if (raiseList[i] == method) return;
-		raiseList.push(method);
-	};
-	//Enfernt eine Methode , die durch addUpdateHandler hinzugefügt wurde.
-	//method: function - Die ehemals hinzugefügt Methode
-	this.removeUpdateHandler = function(method) {
-		for (var i = 0; i<raiseList.length; ++i)
-			if (raiseList[i] == method) {
-				raiseList.splice(i, 1);
-				return;
-			}
-	};
+	//[EVENT] Dieses Event wird ausgelöst, wenn sich der Wert dieser Variable ändert.
+	//[function(sender)]
+	//    sender: Objekt - dieses Objekt
+	Object.defineProperty(this, "UpdatedEvent", {get: function(){ return updadedEvent; } });
 	//Setzt den Standartwert für die Property
 	//value: Wert - Der Standartwert
 	this.setDefaultValue = function(value) {
@@ -86,14 +112,12 @@ Marking.Editor.UpdateProperty = function(owner, key, path) {
 	//newValue: Wert - Der neue Wert der Property
 	this.changeValue = function(newValue) {
 		owner[key] = newValue;
-		for (var i = 0; i<raiseList.length; ++i)
-			raiseList[i](thisref);
+		updatedEvent.invoke(thisref);
 	};
 	//Setzt den Wert auf den Standart zurück
 	this.resetValue = function() {
 		owner[key] = defaultValue;
-		for (var i = 0; i<raiseList.length; ++i)
-			raiseList[i](thisref);
+		updatedEvent.invoke(thisref);
 	};
 	
 };
@@ -102,46 +126,32 @@ Marking.Editor.UpdateProperty = function(owner, key, path) {
 //               diesem Objekt werden auch hier wieder verfügbar gemacht. Obwohl dieses Objekt als Daten-
 //               speicher dient, sollten alle Zugriffe nur über das UpdateObject erfolgen
 //path: Objekt - Eindeutige Informationen, die das Objekt identifizieren
-Marking.Editor.UpdateObject = function (data, path) {
+MarkingTool.Editor.UpdateObject = function (data, path) {
 	var propertys = {};
-	var raiseList = [];
 	var thisref = this;
-	//Objekt - Die eindeitige Information, die dieses Objekt identifiziert.
-	this.path = path;
+	var updadedEvent = new MarkingTool.Event();
 	var raiseHandler = function(sender) {
-		for (var i = 0; i<raiseList.length; ++i)
-			raiseList[i](thisref);
+		updatedEvent.invoke(thisref);
 	};
 	for (var key in data) {
 		if (data.hasOwnProperty(key)) {
-			propertys[key] = new Marking.Editor.UpdateProperty(data, key, path);
+			propertys[key] = new MarkingTool.Editor.UpdateProperty(data, key, path);
 			Object.defineProperty(this, key, {
 				get: function() { return data[key]; },
 				set: function(value) { thisref.propertys[key].changeValue(value); }
 			});
-			propertys[key].addUpdateHandler(raiseHandler);
+			propertys[key].UpdatedEvent.add(raiseHandler);
 		}
 		else this[key] = data[key];
 	}
-	//Fügt eine neue Methode hinzu die aufgerufen wird, sobald der Wert einer Variable sich ändert.
-	//method: function(sender) - Die Methode
-	//        sender: Objekt   - Das Objekt welches diesen Event ausgelöst hat (meist dieses)
-	this.addUpdateHandler = function(method) {
-		for (var i = 0; i<raiseList.length; ++i)
-			if (raiseList[i] == method) return;
-		raiseList.push(method);
-	};
-	//Enfernt eine Methode , die durch addUpdateHandler hinzugefügt wurde.
-	//method: function - Die ehemals hinzugefügt Methode
-	this.removeUpdateHandler = function(method) {
-		for (var i = 0; i<raiseList.length; ++i)
-			if (raiseList[i] == method) {
-				raiseList.splice(i, 1);
-				return;
-			}
-	};
+	//Objekt - Die eindeitige Information, die dieses Objekt identifiziert.
+	this.path = path;
+	//[EVENT] Dieses Event wird ausgelöst, wenn sich der Wert einer Variable ändert.
+	//[function(sender)]
+	//    sender: Objekt - dieses Objekt
+	Object.defineProperty(this, "UpdatedEvent", {get: function(){ return updadedEvent; } });
 	//Setzt alle Propertys auf ihren Standartwert
-	this.resetValue = function() {
+	this.resetValues = function() {
 		for (var key in propertys)
 			if (propertys.hasOwnProperty(key))
 				propertys[key].resetValue();
@@ -167,9 +177,79 @@ Marking.Editor.UpdateObject = function (data, path) {
 		for (var key in propertys)
 			if (propertys.hasOwnProperty(key))
 				propertys[key].setDefaultValue(data[key]);
+		updatedEvent.invoke(thisref);
+	};
+	//Ruft die Liste alle Propertys zu diesen Objekt ab
+	//return: Objekt - Eine Liste aus Schlüssel-Werte-Paaren mit UpdateProperty als Werten.
+	this.getPropertys = function() {
+		return propertys;
 	};
 };
-
-
+//Eine Bibliothek die mehrere Objekte auf Änderungen überwachen kann.
+MarkingTool.Editor.UpdateFactory = new function() {
+	var thisref = this;
+	var changedHandler = function(sender) {
+		if (sender.isValueChanged()) {
+			for (var i = 0; i<thisref.UpdateList; ++i)
+				if (thisref.UpdateList[i] == sender)
+					return;
+			thisref.UpdateList.push(sender);
+			addedEvent.invoke(sender);
+		}
+		else {
+			for (var i = 0; i<thisref.UpdateList; ++i)
+				if (thisref.UpdateList[i] == sender) {
+					thisref.UpdateList.splice(i, 1);
+					removedEvent.invoke(sender);
+					return;
+				}
+		}
+	};
+	var addedEvent = new MarkingTool.Event();
+	var removedEvent = new MarkingTool.Event();
+	
+	//[EVENT] Dieses Event wird ausgelöst, falls eines Objekt sich geändert hat und nun in UpdateList steht.
+	//[function(sender)]
+	//    sender: Objekt - das veränderte Objekt
+	Object.defineProperty(this, "AddedEvent", {get: function() { return addedEvent; } });
+	//[EVENT] Dieses Event wird ausgelöst, falls eines Objekt nun nicht mehr als geändert betrachtet wird und 
+	//        deshalb nicht mehr in UpdateList steht.
+	//[function(sender)]
+	//    sender: Objekt - das nun unveränderte Objekt
+	Object.defineProperty(this, "RemovedEvent", {get: function() { return addedEvent; } });
+	//[UpdateObject] - Eine Liste aller aktuell geänderter Werte
+	this.UpdateList = [];
+	//[UpdateObject] - Eine Liste aller Objekte die von dieser Facoty überwacht werden.
+	this.WatchList = [];
+	//Fügt ein neues Objekt der Überwachung hinzu.
+	//data:   Objekt       - Die Daten mit den das UpdateObject erstellt wird.
+	//path:   Objekt       - Die Daten mit den das Objekt identifiziert wird.
+	//return: UpdateObject - Das zur Überwachung hinzugefügte UpdateObject
+	this.AddObject = function(data, path) {
+		var obj = new MarkingTool.Editor.UpdateObject(data, path);
+		obj.addUpdateHandler(changedHandler);
+		WatchList.push(obj);
+		return obj;
+	};
+	//Entfernt ein UpdateObject wieder aus der Überwachung.
+	//path: Objekt - der Identifizierer für das UpdateObject.
+	this.RemoveObject = function(path) {
+		for (var i = 0; i<this.WatchList.length; ++i)
+			if (this.WatchList[i].path == path) {
+				this.WatchList.splice(i,1)[0].removeUpdateHandler(changedHandler);
+				return;
+			}
+	};
+	//Setzt ein UpdateObject wieder auf den Standartwert zurück.
+	//path: Objekt - der Identifizierer unter der das UpdateObject zu finden ist.
+	this.Reset = function(path) {
+		for (var i = 0; i<this.UpdateList.length; ++i)
+			if (this.UpdateList[i].path == path) {
+				this.UpdateList.splice(i, 1)[0].resetValues();
+				return;
+			}
+	};
+	
+};
 
 
