85f74e7c4586124788a8b6c9a18f1969c87085e5
Till Uhlig <till.uhlig@student.uni-halle.de>
2017-02-22 00:09:57 +0100

    wenn die StudIPAuthentication den neuen Nutzer nicht anlegen kann, dann versucht sie ihn wenigstens zu aktualisieren (mit der externalId)

--- UI/include/StudIPAuthentication.php
+++ UI/include/StudIPAuthentication.php
@@ -290,6 +290,25 @@ class StudIPAuthentication extends AbstractAuthentication
             $user = User::decodeUser($answer);
             if ($user->getStatus()== '201')
                 return true;
+        } else {
+            // wenn der Nutzer nicht angelegt werden konnte, dann existiert er vielleicht schon
+            $username = $data->getUserName();
+            $url = "{$databaseURI}/user/user/{$username}";
+            $message=null;
+            $user = http_get($url, false, $message);
+            $user = json_decode($user, true);
+            // check if user exists in our system
+            if ($message != "404" && empty($user) == false) {
+                // der Nutzer existiert schon (also updaten wir ihn)
+                $userUpdate = User::createUser(null,null,null,null,null,NULL,null,null,null,null,$data->getExternalId());
+                $url = "{$databaseURI}/user/user/".$user['id'];
+                $message = null;
+                $answer = http_put_data($url, User::encodeUser($userUpdate), false, $message);
+                
+                if ($message=='201'){
+                    return true;
+                }
+            }
         }
         return false;
     }
@@ -346,7 +365,7 @@ class StudIPAuthentication extends AbstractAuthentication
         // prüfe den Wartungsmodus
         global $maintenanceMode;
         global $maintenanceText;
-        global $maintenanceAllowedUsers;
+        global $maintenanceAllowedUsers; // hier bezieht sich unsername aber auf die externe Bezeichnung von Studip
         if ($maintenanceMode === '1' && !in_array($username,explode(',',str_replace(' ', '', $maintenanceAllowedUsers)))){
             $text = $maintenanceText;
             if (trim($maintenanceText) == '') $text = "Wartungsarbeiten!!!";
