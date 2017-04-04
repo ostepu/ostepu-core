65f8c63b9208b4d5c37130650bdc602ab848df9f
Max Brauer <ma.brauer@live.de>
2017-04-04 15:29:32 +0200

    Mantis wurde angegeben und in der Serienansicht der Text geändert.

--- UI/javascript/marking_tool2_editor.js
+++ UI/javascript/marking_tool2_editor.js
@@ -388,7 +388,8 @@ MarkingTool.Editor.View = new function() {
 					hc.CreateElement("div", "Warnung", {css: ["warning-header"]}),
 					hc.CreateElement("div", "Dieser Bestandteil der Plattform befindet sich noch im aktiven Entwicklungsstadium, "+
 					"weshalb einige Dinge unter Umständen noch nicht funktionieren oder sich in Zukunft sehr stark "+
-					"verändern werden.")
+					"verändern werden.<br/>Probleme und Anmerkungen hier melden: <a href=\"http://www3.informatik.uni-halle.de/mantis/\""+
+					">Mantis</a>")
 				] 
 			}),
 			hc.CreateElementRaw({
