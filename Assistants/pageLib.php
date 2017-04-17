60fcd514e99f59d49181ac74df011a96f3f3078a
Till Uhlig <till.uhlig@student.uni-halle.de>
2017-04-17 12:01:29 +0200

    Kommentare eingefügt

--- Assistants/pageLib.php
+++ Assistants/pageLib.php
@@ -6,10 +6,12 @@ include_once ( dirname(__FILE__) . '/Model.php' );
 class pageLib
 {
     public static function getContent($linkName, &$model, $confFile){
+        // wenn die Konfigurationsdatei nicht existiert, dann können wir hier aufhören
         if (!file_exists($confFile)){
             return '';
         }
 
+        // hier wird die Komponente initialisiert
         $component = new Model('', dirname($confFile), null);   
         $component->_conf=CConfig::loadStaticConfig('','',dirname($confFile),basename($confFile));
         $component->_com=new CConfig('');
@@ -20,9 +22,9 @@ class pageLib
         $positive2 = function($input, &$res, &$model) {
             $input = json_decode($input,true);
             $res[] = $input['content'];
-            if (isset($input['model'])){
+            /*if (isset($input['model'])){
                 $model = array_merge($model, $input['model']);
-            }
+            }*/
         };
         
         $negative2 = function(&$model, $linkName) {
@@ -37,6 +39,7 @@ class pageLib
         // ruft alle Views auf
         $component->callAll('getContent', array(), json_encode($model), 200, $positive2, array('res'=>&$res, 'model'=>&$model), $negative2, array('model'=>&$model, 'linkName'=>$linkName));
 
+        // das Ergebnis der Aufrufe wird nun zu einem String zusammengefasst
         return implode('',$res);
     }
 }
