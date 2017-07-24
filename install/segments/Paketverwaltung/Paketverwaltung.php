a5f01b734d3e616f6de80b8ef89e243c92d9058c
Till Uhlig <till.uhlig@student.uni-halle.de>
2017-07-24 17:46:13 +0200

    im Installationsassistenten-Paketverwaltung, wurde bei der Ermittlung der Dateien eines Pakets die Eliminierung der Duplikate verbessert (es werden nun weniger Dateien erkannt)

--- install/segments/Paketverwaltung/Paketverwaltung.php
+++ install/segments/Paketverwaltung/Paketverwaltung.php
@@ -857,7 +857,14 @@ class Paketverwaltung {
         }
 
         if ($fileList !== null){
-            $fileList = array_unique($fileList);
+            // hier werden alle doppelten Elemente entfernt (indem das Array neu aufgebaut wird)
+            $tempFileList = array();
+            foreach($fileList as $item){
+                $name = strtolower(trim($item));
+                $name = str_replace("\\","/",$name);
+                $tempFileList[$name] = $item;
+            }
+            $fileList = array_values($tempFileList);
         }
 
         Installation::log(array('text' => Installation::Get('main', 'functionEnd')));
